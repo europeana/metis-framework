76741d9e4e3adeba8aca3a0233c86af4f4e5dd7a
ymamakis <yorgos.mamakis@europeana.eu>
2016-07-20 10:36:52 +0200

    Added new method for downloading the xsl for a mapping

--- metis-pandora/metis-pandora-rest/src/main/java/eu/europeana/metis/framework/rest/controllers/XslController.java
+++ metis-pandora/metis-pandora-rest/src/main/java/eu/europeana/metis/framework/rest/controllers/XslController.java
@@ -1,16 +1,23 @@
 package eu.europeana.metis.framework.rest.controllers;
 
+import eu.europeana.metis.mapping.exceptions.MappingNotFoundException;
 import eu.europeana.metis.mapping.exceptions.MappingToXSLException;
 import eu.europeana.metis.mapping.model.Mapping;
+import eu.europeana.metis.service.MongoMappingService;
 import eu.europeana.metis.service.XSLTGenerationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * A Controller providing a method to convert a Mapping to an XSL
@@ -22,6 +29,8 @@ public class XslController {
 
     @Autowired
     private XSLTGenerationService generationService;
+    @Autowired
+    private MongoMappingService mappingService;
 
     /**
      * Generate an XSL from a Mapping
@@ -39,4 +48,30 @@ public class XslController {
         }
     }
 
+    /**
+     * Download the XSL for a mapping (it will be automatically generated if it has not been created in the past)
+     * @param mappingId The mapping id
+     * @param response The HttpServletResponse
+     * @throws MappingNotFoundException
+     * @throws IOException
+     * @throws MappingToXSLException
+     */
+    @ApiOperation(value="Download the xsl for a mapping")
+    @ResponseBody
+    @RequestMapping(value="/xsl/{mappingId}", method = RequestMethod.GET)
+    public void downloadXslForMapping(@PathVariable(value="mappingId") String mappingId, HttpServletResponse response)
+            throws MappingNotFoundException, IOException, MappingToXSLException {
+        Mapping mapping = mappingService.getByid(mappingId);
+        if(mapping==null){
+            throw new MappingNotFoundException(mappingId);
+        }
+        byte[] xslBytes = mapping.getXsl()==null?createXSLFromMapping(mapping).getBytes():mapping.getXsl().getBytes();
+        ByteArrayInputStream bain = new ByteArrayInputStream(xslBytes);
+        String mimeType = "application/octet-stream";
+        response.setContentType(mimeType);
+        response.setHeader("Content-Disposition", String.format("inline; filename=\"" + mapping.getName() + ".xsl\""));
+        response.setContentLength(mapping.getXsl().length());
+        InputStream inputStream = new BufferedInputStream(bain);
+        FileCopyUtils.copy(inputStream, response.getOutputStream());
+    }
 }
