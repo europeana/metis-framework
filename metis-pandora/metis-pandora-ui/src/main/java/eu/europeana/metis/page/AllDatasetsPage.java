dde3b57acfe7583b4d95e5441b466e93c3cde927
alena-fedasenka <alena.fedasenka@kb.nl>
2016-10-25 17:18:25 +0200

    Incremental commit.

---
+++ metis-pandora/metis-pandora-ui/src/main/java/eu/europeana/metis/page/AllDatasetsPage.java
@@ -0,0 +1,30 @@
+package eu.europeana.metis.page;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
+
+import eu.europeana.metis.common.MetisPage;
+
+public class AllDatasetsPage extends MetisPage {
+
+	@Override
+	public Byte resolveCurrentPage() {
+		return 1;
+	}
+
+	@Override
+	public void addPageContent(Map<String, Object> model) {
+		//TODO
+	}
+
+	@Override
+	public List<Entry<String, String>> resolveBreadcrumbs() {
+		List<Entry<String, String>> breadcrumbs = new ArrayList<>();
+		breadcrumbs.add(new SimpleEntry<String, String>("Home", "/home-page"));
+		breadcrumbs.add(new SimpleEntry<String, String>("All Datasets", "#"));
+		return breadcrumbs;
+	}
+}
