a10a390f0a7dc7b6936809061e086ad9e1891c2e
stzanakis <simon.tzanakis@europeana.eu>
2017-02-20 09:54:44 +0100

    RE #2355 : Create EcloudDatasetDao and update MetisDao interface

--- metis-core/metis-framework-service/src/main/java/eu/europeana/metis/framework/dao/MetisDao.java
+++ metis-core/metis-framework-service/src/main/java/eu/europeana/metis/framework/dao/MetisDao.java
@@ -21,32 +21,32 @@ package eu.europeana.metis.framework.dao;
  * Interface specifying the minimum methods for a DAO persisting
  * Created by ymamakis on 2/17/16.
  */
-public interface MetisDao<T> {
+public interface MetisDao<T, S> {
 
     /**
      * Create a dataset or organization
      * @param t The dataset or organization to create
      */
-    void create(T t);
+    S create(T t);
 
     /**
      * Update a dataset or organization
      * @param t The dataset or organization to update
      */
-    void update(T t);
+    S update(T t);
 
     /**
      * Get a dataset or organization by id
      * @param id The id to look for
      * @return The entity to return
      */
-    T getById(String id);
+    T getById(S id);
 
     /**
      * Delete an entity form the database
      * @param t
      */
-    void delete(T t);
+    boolean delete(T t);
 
 
 }
---
+++ metis-core/metis-framework-service/src/main/java/eu/europeana/metis/framework/dao/ecloud/EcloudDatasetDao.java
@@ -0,0 +1,102 @@
+package eu.europeana.metis.framework.dao.ecloud;
+
+import eu.europeana.cloud.common.model.DataSet;
+import eu.europeana.cloud.mcs.driver.DataSetServiceClient;
+import eu.europeana.cloud.service.mcs.exception.MCSException;
+import eu.europeana.metis.framework.dao.MetisDao;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * @author Simon Tzanakis (Simon.Tzanakis@europeana.eu)
+ * @since 2017-02-20
+ */
+public class EcloudDatasetDao implements MetisDao<DataSet, String>{
+
+  private final Logger LOGGER = LoggerFactory.getLogger(EcloudDatasetDao.class);
+
+  @Autowired
+  private DataSetServiceClient dataSetServiceClient;
+
+  @Value("${ecloud.provider}")
+  private String ecloudProvider;
+
+  @Override
+  public String create(DataSet dataSet) {
+    try {
+      dataSetServiceClient.createDataSet(ecloudProvider, dataSet.getId(), dataSet.getDescription());
+      LOGGER.info("Dataset '" + dataSet.getId() + "' created with Provider '" + ecloudProvider + "' and Description '" + dataSet.getDescription() + "' in ECloud");
+    } catch (MCSException ex) {
+      LOGGER.error("Provider '" + ecloudProvider + "'could not create Dataset '" + dataSet.getId()
+          + "'in ECloud", ex);
+    }
+    return null;
+  }
+
+  @Override
+  public String update(DataSet dataSet) {
+    try {
+      dataSetServiceClient
+          .updateDescriptionOfDataSet(ecloudProvider, dataSet.getId(), dataSet.getDescription());
+      LOGGER.info("Dataset '" + dataSet.getId() + "' updated with Provider '" + ecloudProvider + "' and Description '" + dataSet.getDescription() + "' in ECloud");
+    } catch (MCSException ex) {
+      LOGGER.error(
+          "Provider '" + ecloudProvider + "' could not update description of Dataset '" + dataSet.getId()
+              + "' in ECloud", ex);
+    }
+    return null;
+  }
+
+  @Override
+  public DataSet getById(String dataSetId) {
+    // TODO: 20-2-17 Update when ecloud has direct call to retrieve dataset by id
+    List<DataSet> dataSetsForProvider = null;
+    try {
+      dataSetsForProvider = dataSetServiceClient
+          .getDataSetsForProvider(ecloudProvider);
+    } catch (MCSException ex) {
+      LOGGER.error("Could not retrieve datasets of Provider '" + ecloudProvider + "' from ECloud", ex);
+    }
+    if(dataSetsForProvider != null) {
+      for (DataSet dataSetInEcloud : dataSetsForProvider) {
+        if (dataSetInEcloud.getId().equals(dataSetId))
+          return dataSetInEcloud;
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public boolean delete(DataSet dataSet) {
+    try {
+      dataSetServiceClient.deleteDataSet(ecloudProvider, dataSet.getId());
+      LOGGER.info("Dataset '" + dataSet.getId() + "' deleted with Provider '" + ecloudProvider + "' from ECloud");
+    } catch (MCSException ex) {
+      LOGGER.error("Provider '" + ecloudProvider + "' could not delete Dataset '" + dataSet.getId()
+          + "' in ECloud", ex);
+    }
+    return false;
+  }
+
+  public boolean exists(String dataSetId) {
+    // TODO: 20-2-17 Update when ecloud has direct call to exist or retrieve dataset by id
+    List<DataSet> dataSetsForProvider = null;
+    try {
+      dataSetsForProvider = dataSetServiceClient
+          .getDataSetsForProvider(ecloudProvider);
+    } catch (MCSException ex) {
+      LOGGER.error("Could not retrieve datasets of Provider '" + ecloudProvider + "' from ECloud",
+          ex);
+    }
+    if (dataSetsForProvider != null) {
+      for (DataSet dataSetInEcloud : dataSetsForProvider) {
+        if (dataSetInEcloud.getId().equals(dataSetId))
+          return true;
+      }
+    }
+    return false;
+  }
+}
