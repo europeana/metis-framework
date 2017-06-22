c8e507e59087ed36536bceafb86668fb4138befc
stzanakis <simon.tzanakis@europeana.eu>
2017-06-22 17:40:48 +0200

    Cleanup

--- metis-core/metis-core-account/src/main/java/eu/europeana/metis/ui/ldap/utils/LdapTreeVisitor.java
+++ metis-core/metis-core-account/src/main/java/eu/europeana/metis/ui/ldap/utils/LdapTreeVisitor.java
@@ -4,5 +4,5 @@ import org.springframework.ldap.core.DirContextOperations;
 
 public interface LdapTreeVisitor {
 
-	public void visit(DirContextOperations node, int currentDepth);
+	void visit(DirContextOperations node, int currentDepth);
 }
