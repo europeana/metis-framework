41c301e43bd9affc97d26f7fabff3d2932e5526f
stzanakis <simon.tzanakis@europeana.eu>
2017-10-25 13:28:39 +0200

    MET-86 Revove metis-core-account

--- metis-core/metis-core-account/src/main/java/eu/europeana/metis/ui/ldap/utils/LdapTreeVisitor.java
+++
@@ -1,8 +0,0 @@
-package eu.europeana.metis.ui.ldap.utils;
-
-import org.springframework.ldap.core.DirContextOperations;
-
-public interface LdapTreeVisitor {
-
-	void visit(DirContextOperations node, int currentDepth);
-}
