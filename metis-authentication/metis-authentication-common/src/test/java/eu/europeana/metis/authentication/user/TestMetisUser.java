b36ed3c8c07c507a8403ce14ee949738abee31d8
stzanakis <simon.tzanakis@europeana.eu>
2017-11-07 16:52:23 +0100

    MET-141 Update MetisUser tests with file models

--- metis-authentication/metis-authentication-common/src/test/java/eu/europeana/metis/authentication/user/TestMetisUser.java
+++ metis-authentication/metis-authentication-common/src/test/java/eu/europeana/metis/authentication/user/TestMetisUser.java
@@ -2,10 +2,12 @@ package eu.europeana.metis.authentication.user;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import eu.europeana.metis.authentication.exceptions.BadContentException;
 import java.io.File;
-import java.util.Iterator;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,22 +19,13 @@ import org.junit.Test;
 public class TestMetisUser {
 
   private static final String DATA_JSON_NODE_ZOHO_USER_EXAMPLE = "data/jsonNodeZohoUserExample";
+  private static final String DATA_JSON_NODE_ZOHO_USER_IS_ADMIN_EXAMPLE = "data/jsonNodeZohoUserIsAdminExample";
   private static final String DATA_JSON_NODE_ZOHO_ORGANIZATION_EXAMPLE = "data/jsonNodeZohoOrganizationExample";
+  private static final String DATA_JSON_NODE_ZOHO_ORGANIZATION_WRONG_ROLE_EXAMPLE = "data/jsonNodeZohoOrganizationWrongRoleExample";
 
   @Test
   public void metisUserConstructor() throws Exception {
-    File jsonNodeZohoUserExampleFile = new File(getClass().getClassLoader().getResource(
-        DATA_JSON_NODE_ZOHO_USER_EXAMPLE).getFile());
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode jsonNodeZohoUserExample = mapper
-        .readTree(FileUtils.readFileToString(jsonNodeZohoUserExampleFile));
-
-    File jsonNodeZohoOrganizationExampleFile = new File(getClass().getClassLoader().getResource(
-        DATA_JSON_NODE_ZOHO_ORGANIZATION_EXAMPLE).getFile());
-    JsonNode jsonNodeZohoOrganizationExample = mapper
-        .readTree(FileUtils.readFileToString(jsonNodeZohoOrganizationExampleFile));
-
-    MetisUser metisUser = new MetisUser(jsonNodeZohoUserExample.get("FL"));
+    MetisUser metisUser = new MetisUser(getZohoJsonNodeExample(DATA_JSON_NODE_ZOHO_USER_EXAMPLE));
     Assert.assertEquals(true, metisUser.isMetisUserFlag());
     Assert.assertEquals(AccountRole.EUROPEANA_DATA_OFFICER, metisUser.getAccountRole());
     Assert.assertEquals("simon.metis@europeana.eu", metisUser.getEmail());
@@ -40,49 +33,32 @@ public class TestMetisUser {
     Assert.assertNotNull(metisUser.getUserId());
     Assert.assertEquals("Europeana Foundation", metisUser.getOrganizationName());
 
-    metisUser.setOrganizationIdFromJsonNode(jsonNodeZohoOrganizationExample.get("FL"));
+    metisUser.setOrganizationIdFromJsonNode(getZohoJsonNodeExample(DATA_JSON_NODE_ZOHO_ORGANIZATION_EXAMPLE));
     Assert.assertNotNull(metisUser.getOrganizationId());
   }
 
   @Test(expected = BadContentException.class)
   public void metisUserConstructorWithAdminRoleFromZohoFails() throws Exception {
-    File jsonNodeZohoUserExampleFile = new File(getClass().getClassLoader().getResource(
-        DATA_JSON_NODE_ZOHO_USER_EXAMPLE).getFile());
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode jsonNodeZohoUserExample = mapper
-        .readTree(FileUtils.readFileToString(jsonNodeZohoUserExampleFile));
-    Iterator<JsonNode> elements = jsonNodeZohoUserExample.get("FL").elements();
-    while (elements.hasNext()) {
-      JsonNode next = elements.next();
-      if (next.get("val").textValue().equals("Account Role")) {
-        ObjectNode objectNode = (ObjectNode) next;
-        objectNode.remove("val");
-        objectNode.remove("content");
-        objectNode.put("val", "Account Role");
-        objectNode.put("content", "METIS_ADMIN");
-      }
-    }
-    new MetisUser(jsonNodeZohoUserExample.get("FL"));
+    new MetisUser(getZohoJsonNodeExample(DATA_JSON_NODE_ZOHO_USER_IS_ADMIN_EXAMPLE));
   }
 
   @Test(expected = BadContentException.class)
   public void metisUserSetOrganizationWithEmptyRoleFails() throws Exception {
-    File jsonNodeZohoOrganizationExampleFile = new File(getClass().getClassLoader().getResource(
-        DATA_JSON_NODE_ZOHO_ORGANIZATION_EXAMPLE).getFile());
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode jsonNodeZohoOrganizationExample = mapper
-        .readTree(FileUtils.readFileToString(jsonNodeZohoOrganizationExampleFile));
-    Iterator<JsonNode> elements = jsonNodeZohoOrganizationExample.get("FL").elements();
-    while (elements.hasNext()) {
-      JsonNode next = elements.next();
-      if (next.get("val").textValue().equals("Organisation Role")) {
-        ObjectNode objectNode = (ObjectNode) next;
-        objectNode.remove("content");
-        objectNode.put("content", "WRONG_ROLE");
-      }
-    }
     MetisUser metisUser = new MetisUser();
-    metisUser.setOrganizationIdFromJsonNode(jsonNodeZohoOrganizationExample.get("FL"));
+    metisUser.setOrganizationIdFromJsonNode(getZohoJsonNodeExample(DATA_JSON_NODE_ZOHO_ORGANIZATION_WRONG_ROLE_EXAMPLE));
+  }
+
+  private JsonNode getZohoJsonNodeExample(String filePath) throws IOException, URISyntaxException {
+    URL resource = getClass().getClassLoader().getResource(filePath);
+    if (resource != null) {
+      File jsonNodeZohoUserExampleFile = new File(resource.toURI());
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode jsonNodeZohoUserExample = mapper
+          .readTree(FileUtils.readFileToString(jsonNodeZohoUserExampleFile));
+
+      return jsonNodeZohoUserExample.get("FL");
+    }
+    throw new FileNotFoundException();
   }
 
 
