3256bf692ccaf2b04e59245d267187856f8c7f05
jochen-vermeulen <jochen.vermeulen@europeana.eu>
2018-02-09 15:01:17 +0100

    MET-504: Undo a change that screws up (de)serialization.

--- metis-enrichment/metis-enrichment-common/src/main/java/eu/europeana/enrichment/utils/InputValue.java
+++ metis-enrichment/metis-enrichment-common/src/main/java/eu/europeana/enrichment/utils/InputValue.java
@@ -1,29 +1,31 @@
 /*
  * Copyright 2007-2013 The Europeana Foundation
  *
- * Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved by the
- * European Commission; You may not use this work except in compliance with the Licence.
+ *  Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved
+ *  by the European Commission;
+ *  You may not use this work except in compliance with the Licence.
  *
- * You may obtain a copy of the Licence at: http://joinup.ec.europa.eu/software/page/eupl
+ *  You may obtain a copy of the Licence at:
+ *  http://joinup.ec.europa.eu/software/page/eupl
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the Licence
- * is distributed on an "AS IS" basis, without warranties or conditions of any kind, either express
- * or implied. See the Licence for the specific language governing permissions and limitations under
- * the Licence.
+ *  Unless required by applicable law or agreed to in writing, software distributed under
+ *  the Licence is distributed on an "AS IS" basis, without warranties or conditions of
+ *  any kind, either express or implied.
+ *  See the Licence for the specific language governing permissions and limitations under
+ *  the Licence.
  */
 package eu.europeana.enrichment.utils;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import javax.xml.bind.annotation.XmlRootElement;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 /**
- * Enrichment input class wrapper. It defines the basics needed for enrichment as the value to be
- * enriched, the Controlled vocabulary to be used and the field (optional) from which the value
- * originated
+ * Enrichment input class wrapper. It defines the basics needed for enrichment
+ * as the value to be enriched, the Controlled vocabulary to be used and the
+ * field (optional) from which the value originated
  * 
  * @author Yorgos.Mamakis@ europeana.eu
  * 
@@ -32,37 +34,53 @@ import javax.xml.bind.annotation.XmlRootElement;
 @JsonInclude(Include.ALWAYS)
 public class InputValue {
 
-  private String originalField;
+	private String originalField;
 
-  private String value;
+	private String value;
 
-  private String language;
+	private String language;
 
-  private List<EntityClass> vocabularies;
+	private List<EntityClass> vocabularies;
 
-  InputValue() {}
+	public InputValue() {
+	}
 
-  public InputValue(String originalField, String value, String language,
-      EntityClass... vocabularies) {
-    this.originalField = originalField;
-    this.value = value;
-    this.language = language;
-    this.vocabularies = Arrays.asList(vocabularies);
-  }
+	public InputValue(String originalField, String value, String language, EntityClass... vocabularies) {
+	    this.originalField = originalField;
+	    this.value = value;
+	    this.language = language;
+	    this.vocabularies = Arrays.asList(vocabularies);
+	}
 
-  public String getOriginalField() {
-    return originalField;
-  }
+	public String getOriginalField() {
+		return originalField;
+	}
 
-  public String getValue() {
-    return value;
-  }
+	public void setOriginalField(String originalField) {
+		this.originalField = originalField;
+	}
 
-  public List<EntityClass> getVocabularies() {
-    return Collections.unmodifiableList(vocabularies);
-  }
+	public String getValue() {
+		return value;
+	}
 
-  public String getLanguage() {
-    return language;
-  }
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public List<EntityClass> getVocabularies() {
+		return vocabularies;
+	}
+
+	public void setVocabularies(List<EntityClass> vocabularies) {
+		this.vocabularies = vocabularies;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
 }
