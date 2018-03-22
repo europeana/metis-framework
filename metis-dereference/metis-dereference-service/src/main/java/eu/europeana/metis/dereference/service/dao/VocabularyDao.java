73886c7cbbb773408d472172d745e026d0a73ff5
jochen-vermeulen <jochen.vermeulen@europeana.eu>
2018-03-22 11:59:05 +0100

    MET-697: Trigger resolving the resource if we don't have vocabulary.
    Save the vocabulary in the resource cache. Resolve the resource not only
    if we don't have it yet, but also if we don't have a (valid) vocabulary
    for it yet.

--- metis-dereference/metis-dereference-service/src/main/java/eu/europeana/metis/dereference/service/dao/VocabularyDao.java
+++ metis-dereference/metis-dereference-service/src/main/java/eu/europeana/metis/dereference/service/dao/VocabularyDao.java
@@ -10,99 +10,109 @@ import com.mongodb.MongoClient;
 import eu.europeana.metis.dereference.Vocabulary;
 
 /**
- * Dao for vocabularies
- * Created by ymamakis on 2/11/16.
+ * Dao for vocabularies Created by ymamakis on 2/11/16.
  */
 
 public class VocabularyDao {
 
-    private Datastore ds;
+  private Datastore ds;
 
-    public VocabularyDao(MongoClient mongo, String db) {
-        Morphia morphia = new Morphia();
-        morphia.map(Vocabulary.class);
+  public VocabularyDao(MongoClient mongo, String db) {
+    Morphia morphia = new Morphia();
+    morphia.map(Vocabulary.class);
 
-        ds = morphia.createDatastore(mongo, db);
+    ds = morphia.createDatastore(mongo, db);
+  }
+
+  /**
+   * Retrieve the vocabularies for which the URL contains the given search string.
+   *
+   * @param searchString The string to search on
+   * @return The list of vocabularies. Is not null.
+   */
+  public List<Vocabulary> getByUriSearch(String searchString) {
+    final Pattern pattern = Pattern.compile(Pattern.quote(searchString));
+    return ds.find(Vocabulary.class).filter("uri", pattern).asList();
+  }
+
+  /**
+   * Save a vocabulary
+   *
+   * @param entity The vocabulary to save
+   * @return The ID under which the vocabulary was saved.
+   */
+  public String save(Vocabulary entity) {
+    return (String) ds.save(entity).getId();
+  }
+
+  /**
+   * Delete a vocabulary by name
+   *
+   * @param name The name of the vocabulary to delete
+   */
+  public void delete(String name) {
+    ds.delete(ds.createQuery(Vocabulary.class).filter("name", name));
+  }
+
+  /**
+   * Update the mapping of a vocabulary. It will be created if it does not exist
+   *
+   * @param entity The Vocabulary to update
+   */
+  public void update(Vocabulary entity) {
+    Query<Vocabulary> query = ds.createQuery(Vocabulary.class).filter("name", entity.getName());
+    UpdateOperations<Vocabulary> ops = ds.createUpdateOperations(Vocabulary.class);
+
+    ops.set("iterations", entity.getIterations());
+    if (entity.getRules() != null) {
+      ops.set("rules", entity.getRules());
+    } else {
+      ops.unset("rules");
     }
-
-    /**
-     * Retrieve the vocabularies for which the URL contains the given search string.
-     *
-     * @param searchString The string to search on
-     * @return The list of vocabularies. Is not null.
-     */
-    public List<Vocabulary> getByUriSearch(String searchString) {
-        final Pattern pattern = Pattern.compile(Pattern.quote(searchString)); 
-        return ds.find(Vocabulary.class).filter("uri", pattern).asList();
+    if (entity.getTypeRules() != null) {
+      ops.set("typeRules", entity.getTypeRules());
+    } else {
+      ops.unset("typeRules");
     }
-
-    /**
-     * Save a vocabulary
-     *
-     * @param entity The vocabulary to save
-     */
-    public void save(Vocabulary entity) {
-        ds.save(entity);
+    if (entity.getType() != null) {
+      ops.set("type", entity.getType());
     }
+    ops.set("uri", entity.getUri());
+    ops.set("xslt", entity.getXslt());
+    ops.set("suffix", entity.getSuffix());
+    ds.update(query, ops);
+  }
 
-    /**
-     * Delete a vocabulary by name
-     *
-     * @param name The name of the vocabulary to delete
-     */
-    public void delete(String name) {
-        ds.delete(ds.createQuery(Vocabulary.class).filter("name", name));
-    }
+  /**
+   * Retrieve all the vocabularies
+   *
+   * @return A list of all the vocabularies
+   */
+  public List<Vocabulary> getAll() {
+    return ds.find(Vocabulary.class).asList();
+  }
 
-    /**
-     * Update the mapping of a vocabulary. It will be created if it does not exist
-     *
-     * @param entity The Vocabulary to update
-     */
-    public void update(Vocabulary entity) {
-        Query<Vocabulary> query = ds.createQuery(Vocabulary.class).filter("name", entity.getName());
-        UpdateOperations<Vocabulary> ops = ds.createUpdateOperations(Vocabulary.class);
+  /**
+   * Retrieve the vocabulary based on its ID.
+   * 
+   * @param vocabularyId The ID of the vocabulary to retrieve.
+   * @return A list of all the vocabularies
+   */
+  public Vocabulary get(String vocabularyId) {
+    return ds.find(Vocabulary.class).filter("id", vocabularyId).get();
+  }
 
-            ops.set("iterations", entity.getIterations());
-        if(entity.getRules()!=null) {
-            ops.set("rules", entity.getRules());
-        } else {
-            ops.unset("rules");
-        }
-        if(entity.getTypeRules()!=null) {
-            ops.set("typeRules", entity.getTypeRules());
-        } else {
-            ops.unset("typeRules");
-        }
-        if(entity.getType()!=null) {
-            ops.set("type", entity.getType());
-        }
-        ops.set("uri", entity.getUri());
-        ops.set("xslt", entity.getXslt());
-        ops.set("suffix", entity.getSuffix());
-        ds.update(query, ops);
-    }
+  /**
+   * Return a Vocabulary by name
+   *
+   * @param name The name to search on
+   * @return The Vocabulary with that name
+   */
+  public Vocabulary findByName(String name) {
+    return ds.find(Vocabulary.class).filter("name", name).get();
+  }
 
-    /**
-     * Retrieve all the vocabularies
-     *
-     * @return A list of all the vocabularies
-     */
-    public List<Vocabulary> getAll() {
-        return ds.find(Vocabulary.class).asList();
-    }
-
-    /**
-     * Return a Vocabulary by name
-     *
-     * @param name The name to search on
-     * @return The Vocabulary with that name
-     */
-    public Vocabulary findByName(String name) {
-        return ds.find(Vocabulary.class).filter("name", name).get();
-    }
-
-    public void setDs(Datastore ds) {
-        this.ds = ds;
-    }
+  public void setDs(Datastore ds) {
+    this.ds = ds;
+  }
 }
--- metis-dereference/metis-dereference-service/src/test/java/eu/europeana/metis/dereference/service/utils/VocabularyCandidatesTest.java
+++ metis-dereference/metis-dereference-service/src/test/java/eu/europeana/metis/dereference/service/utils/VocabularyCandidatesTest.java
@@ -1,10 +1,12 @@
 package eu.europeana.metis.dereference.service.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,7 +16,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import eu.europeana.metis.dereference.Vocabulary;
 
-public class VocabularyMatchUtilsTest {
+public class VocabularyCandidatesTest {
 
   @Test
   public void findVocabulariesForUrlTest() throws URISyntaxException {
@@ -57,8 +59,8 @@ public class VocabularyMatchUtilsTest {
 
     // Match and obtain list of matching vocabularies
     final String resourceId = correctVocUri + correctUriRule + "/123456";
-    final List<Vocabulary> result =
-        VocabularyMatchUtils.findVocabulariesForUrl(resourceId, vocabularyProviderMock);
+    final List<Vocabulary> result = VocabularyCandidates
+        .findVocabulariesForUrl(resourceId, vocabularyProviderMock).getCandidates();
 
     // Verify that the provider was called exactly once with the right value.
     Mockito.verify(vocabularyProviderMock).apply(Mockito.anyString());
@@ -80,49 +82,99 @@ public class VocabularyMatchUtilsTest {
     // Create mock of vocabulary provider (cannot use lambda as class would be final)
     final Function<String, List<Vocabulary>> vocabularyProvider = new VocabularyProvider(null);
     final String resourceId = "http://dummy.com/123456";
-    final List<Vocabulary> result =
-        VocabularyMatchUtils.findVocabulariesForUrl(resourceId, vocabularyProvider);
+    final VocabularyCandidates result =
+        VocabularyCandidates.findVocabulariesForUrl(resourceId, vocabularyProvider);
 
     // Verify that the provider was called exactly once with the right value.
     assertTrue(result.isEmpty());
   }
 
   @Test
-  public void findVocabularyForTypeTest() {
+  public void processTypeRulesTest() {
 
     // Create vocabularies that should match (with and without rules)
     final String correctTypeIdentifier = "#myCorrectType";
     final String wrongTypeIdentifier = "#myWrongType";
-    final Vocabulary correctVocabulary1 = new Vocabulary();
-    correctVocabulary1.setId("c1");
-    correctVocabulary1
+    final Vocabulary vocWithRightType = new Vocabulary();
+    vocWithRightType.setId("v1");
+    vocWithRightType
         .setTypeRules(new HashSet<>(Arrays.asList(correctTypeIdentifier, wrongTypeIdentifier)));
-    final Vocabulary correctVocabulary2 = new Vocabulary();
-    correctVocabulary2.setId("c2");
+    final Vocabulary vocWithNoType = new Vocabulary();
+    vocWithNoType.setId("v2");
 
     // Create vocabulary that should not match
-    final Vocabulary wrongVocabulary1 = new Vocabulary();
-    wrongVocabulary1.setId("w1");
-    wrongVocabulary1.setTypeRules(new HashSet<>(Arrays.asList(wrongTypeIdentifier)));
+    final Vocabulary vocWithWrongType = new Vocabulary();
+    vocWithWrongType.setId("v3");
+    vocWithWrongType.setTypeRules(new HashSet<>(Arrays.asList(wrongTypeIdentifier)));
 
     // Create the input
     final String incomingDataXml = "abcd" + correctTypeIdentifier + "wxyz";
     final String resourceId = "http://dummy.com/123456";
 
-    // Match with first correct vocabulary
-    final Vocabulary result1 = VocabularyMatchUtils.findVocabularyForType(
-        Arrays.asList(correctVocabulary1, wrongVocabulary1), incomingDataXml, resourceId);
-    assertEquals(correctVocabulary1.getId(), result1.getId());
+    // Match with vocabulary with correct type
+    final VocabularyCandidates candidates1 =
+        new VocabularyCandidates(resourceId, Arrays.asList(vocWithRightType, vocWithWrongType));
+    final Vocabulary result1 = candidates1.findVocabularyForType(incomingDataXml);
+    assertEquals(vocWithRightType.getId(), result1.getId());
+    assertNull(candidates1.findVocabularyWithoutTypeRules());
 
-    // Match with second correct vocabulary
-    final Vocabulary result2 = VocabularyMatchUtils.findVocabularyForType(
-        Arrays.asList(wrongVocabulary1, correctVocabulary2), incomingDataXml, resourceId);
-    assertEquals(correctVocabulary2.getId(), result2.getId());
+    // Match with vocabulary without type
+    final VocabularyCandidates candidates2 =
+        new VocabularyCandidates(resourceId, Arrays.asList(vocWithWrongType, vocWithNoType));
+    final Vocabulary result2 = candidates2.findVocabularyForType(incomingDataXml);
+    assertEquals(vocWithNoType.getId(), result2.getId());
+    assertEquals(vocWithNoType.getId(), candidates2.findVocabularyWithoutTypeRules().getId());
 
     // Match without matching vocabularies
-    final Vocabulary result3 = VocabularyMatchUtils
-        .findVocabularyForType(Arrays.asList(wrongVocabulary1), incomingDataXml, resourceId);
+    final VocabularyCandidates candidates3 =
+        new VocabularyCandidates(resourceId, Arrays.asList(vocWithWrongType));
+    final Vocabulary result3 = candidates3.findVocabularyForType(incomingDataXml);
     assertNull(result3);
+    assertNull(candidates3.findVocabularyWithoutTypeRules());
+  }
+
+  @Test
+  public void testIsEmpty() {
+    final Vocabulary vocabulary = new Vocabulary();
+    vocabulary.setId("vId");
+    assertTrue(new VocabularyCandidates("r1", Collections.emptyList()).isEmpty());
+    assertFalse(new VocabularyCandidates("r2", Arrays.asList(vocabulary)).isEmpty());
+  }
+
+  @Test
+  public void testGetSuffixes() {
+
+    // Creat vocabularies with suffixes
+    final String suffixA = "sa";
+    final String suffixB = "sb";
+    final Vocabulary vocabulary1 = new Vocabulary();
+    vocabulary1.setId("v1");
+    vocabulary1.setSuffix(suffixA);
+    final Vocabulary vocabulary2 = new Vocabulary();
+    vocabulary2.setId("v2");
+    vocabulary2.setSuffix(suffixB);
+    final Vocabulary vocabulary3 = new Vocabulary();
+    vocabulary3.setId("v3");
+    vocabulary3.setSuffix(suffixA);
+
+    // Try with all vocabularies
+    final Set<String> suffixes1 =
+        new VocabularyCandidates("r1", Arrays.asList(vocabulary1, vocabulary2, vocabulary3))
+            .getCandidateSuffixes();
+    assertEquals(2, suffixes1.size());
+    assertTrue(suffixes1.contains(suffixA));
+    assertTrue(suffixes1.contains(suffixB));
+
+    // Try with one vocabulary
+    final Set<String> suffixes2 =
+        new VocabularyCandidates("r2", Arrays.asList(vocabulary1)).getCandidateSuffixes();
+    assertEquals(1, suffixes2.size());
+    assertTrue(suffixes2.contains(suffixA));
+
+    // Try with no vocabularies
+    final Set<String> suffixes3 =
+        new VocabularyCandidates("r3", Collections.emptyList()).getCandidateSuffixes();
+    assertTrue(suffixes3.isEmpty());
   }
 
   private static class VocabularyProvider implements Function<String, List<Vocabulary>> {
