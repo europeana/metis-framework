f029e9cf8888eb7b38ce368bc8d5e5a5ca320726
stzanakis <simon.tzanakis@europeana.eu>
2018-04-10 15:51:45 +0200

    MET-596 Bug, add the missing @Controller annotation on the ScheduleWorkflowController.

--- metis-core/metis-core-rest/src/main/java/eu/europeana/metis/core/rest/ScheduleWorkflowController.java
+++ metis-core/metis-core-rest/src/main/java/eu/europeana/metis/core/rest/ScheduleWorkflowController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author Simon Tzanakis (Simon.Tzanakis@europeana.eu)
  * @since 2018-04-05
  */
+@Controller
 public class ScheduleWorkflowController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ScheduleWorkflowController.class);
