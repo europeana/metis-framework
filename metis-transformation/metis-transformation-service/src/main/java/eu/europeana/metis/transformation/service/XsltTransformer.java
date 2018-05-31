4ee2b0fb0b4dda09e1234a85c1c4a1654a1f530b
jochen-vermeulen <jochen.vermeulen@europeana.eu>
2018-05-31 13:03:45 +0200

    MET-1102: No longer share XSL transformers between threads.

--- metis-transformation/metis-transformation-service/src/main/java/eu/europeana/metis/transformation/service/XsltTransformer.java
+++ metis-transformation/metis-transformation-service/src/main/java/eu/europeana/metis/transformation/service/XsltTransformer.java
@@ -1,84 +1,126 @@
 package eu.europeana.metis.transformation.service;
 
-import net.sf.saxon.TransformerFactoryImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.xml.transform.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.URL;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.*;
-import java.net.URL;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.sf.saxon.TransformerFactoryImpl;
 
 /**
- * Created by pwozniak on 3/21/18
+ * This class performs XSL transforms (XSLT). Instances of this class are <b>not thread-safe</b>.
+ * For each thread a new instance needs to be created, but, due to the caching mechanism of the XSLT
+ * compilation, this operation is not very expensive.
  */
 public class XsltTransformer {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(XsltTransformer.class);
 
   private static final int CACHE_SIZE = 50;
-  private static LRUCache<String, Transformer> cache = new LRUCache<>(CACHE_SIZE);
+  private static LRUCache<String, Templates> cache = new LRUCache<>(CACHE_SIZE);
+
+  private final Transformer transformer;
 
   /**
-   * Transforms given file based on a provided url to xslt file.
-   *
-   * @param xsltUrl url to xslt file
-   * @param fileContent content of the file
-   * @return transformed file
-   * @throws TransformationException if a problem occurred during transformation
+   * Constructor in the case that no value of the datasetId field needs to be set.
+   * 
+   * @param xsltUrl The URL of the XSLT file.
+   * @throws TransformationException In case there was a problem setting up the transformation.
    */
-  public StringWriter transform(String xsltUrl, byte[] fileContent) throws TransformationException {
-    return transform(xsltUrl, fileContent, null);
+  public XsltTransformer(String xsltUrl) throws TransformationException {
+    this(xsltUrl, null);
   }
 
   /**
-   * Transforms given file based on a provided url to xslt file.
-   *
-   * @param xsltUrl url to xslt file
-   * @param fileContent content of the file
-   * @param datasetIdValue value that will be injected to datasetId field
-   * @return transformed file
-   * @throws TransformationException if a problem occurred during transformation
+   * Constructor.
+   * 
+   * @param xsltUrl The URL of the XSLT file.
+   * @param datasetId The value that will be injected to the datasetId field in the XSLT.
+   * @throws TransformationException In case there was a problem with setting up the transformation.
    */
-  public StringWriter transform(String xsltUrl, byte[] fileContent, String datasetIdValue)
-      throws TransformationException {
-    try (final InputStream xsltStream = new URL(xsltUrl).openStream()) {
-      final Transformer transformer = getTransformer(xsltUrl, xsltStream);
-      addParameter(transformer, "datasetId", datasetIdValue);
-      try (final InputStream contentStream = new ByteArrayInputStream(fileContent)) {
-        return executeTransformation(transformer, contentStream);
-      }
-    } catch (IOException | TransformerException e) {
+  public XsltTransformer(String xsltUrl, String datasetId) throws TransformationException {
+    try {
+      this.transformer = getTemplates(xsltUrl).newTransformer();
+    } catch (TransformerConfigurationException | IOException e) {
+      LOGGER.error("Exception during transformation setup", e);
+      throw new TransformationException(e);
+    }
+    if (!StringUtils.isBlank(datasetId)) {
+      transformer.setParameter("datasetId", datasetId);
+    }
+  }
+
+  /**
+   * Transforms a file using this instance's XSL transformation.
+   * 
+   * @param fileContent The file to be transformed.
+   * @return The transformed file.
+   * @throws TransformationException In case there was a problem with the transformation.
+   */
+  public StringWriter transform(byte[] fileContent) throws TransformationException {
+    try (final InputStream contentStream = new ByteArrayInputStream(fileContent)) {
+      final StringWriter result = new StringWriter();
+      transformer.transform(new StreamSource(contentStream), new StreamResult(result));
+      return result;
+    } catch (TransformerException | IOException e) {
       LOGGER.error("Exception during transformation", e);
       throw new TransformationException(e);
     }
   }
 
-  private Transformer getTransformer(String xsltUrl, InputStream xsltStream)
-      throws TransformerConfigurationException {
+  /**
+   * Transforms given file based on a provided XSLT. This method is a wrapper of
+   * {@link #transform(String, byte[], String)} for the case no value needs to be set for the
+   * datasetId field. Please see the comments at {@link #transform(String, byte[], String)}.
+   *
+   * @param xsltUrl The URL of the XSLT file.
+   * @param fileContent The file to be transformed.
+   * @return The transformed file.
+   * @throws TransformationException if a problem occurred during transformation
+   */
+  public static StringWriter transform(String xsltUrl, byte[] fileContent)
+      throws TransformationException {
+    return transform(xsltUrl, fileContent, null);
+  }
+
+  /**
+   * Transforms given file based on a provided XSLT. This method creates an instance of this class
+   * that will be discarded after the completion of this method. If multiple files are to be
+   * transformed, it is more efficient to manually create an instance of this class using one of the
+   * constructors and then call its {@link #transform(byte[])} method for each of those files.
+   *
+   * @param xsltUrl The URL of the XSLT file.
+   * @param fileContent The file to be transformed.
+   * @param datasetId The value that will be injected to the datasetId field in the XSLT.
+   * @return The transformed file.
+   * @throws TransformationException if a problem occurred during transformation
+   */
+  public static StringWriter transform(String xsltUrl, byte[] fileContent, String datasetId)
+      throws TransformationException {
+    return new XsltTransformer(xsltUrl, datasetId).transform(fileContent);
+  }
+
+  private static Templates getTemplates(String xsltUrl)
+      throws TransformerConfigurationException, IOException {
     if (cache.containsKey(xsltUrl)) {
       return cache.get(xsltUrl);
-    } else {
-      Source xslDoc = new StreamSource(xsltStream);
-      TransformerFactory transformerFactory = new TransformerFactoryImpl();
-      Transformer transformer = transformerFactory.newTransformer(xslDoc);
-      cache.put(xsltUrl, transformer);
-      return transformer;
     }
-  }
-
-  private StringWriter executeTransformation(Transformer transformer, InputStream contentStream)
-      throws TransformerException {
-    Source xmlDoc = new StreamSource(contentStream);
-    StringWriter writer = new StringWriter();
-    transformer.transform(xmlDoc, new StreamResult(writer));
-    return writer;
-  }
-
-  private void addParameter(Transformer transformer, String parameterName, String parameterValue) {
-    if (parameterValue != null && !parameterValue.isEmpty()) {
-      transformer.setParameter(parameterName, parameterValue);
+    final TransformerFactory transformerFactory = new TransformerFactoryImpl();
+    final Templates templates;
+    try (final InputStream xsltStream = new URL(xsltUrl).openStream()) {
+      templates = transformerFactory.newTemplates(new StreamSource(xsltStream));
     }
+    cache.put(xsltUrl, templates);
+    return templates;
   }
 }
--- metis-transformation/metis-transformation-service/src/test/java/eu/europeana/metis/transformation/service/XsltTransformerTest.java
+++ metis-transformation/metis-transformation-service/src/test/java/eu/europeana/metis/transformation/service/XsltTransformerTest.java
@@ -1,3 +1,4 @@
+package eu.europeana.metis.transformation.service;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import eu.europeana.metis.transformation.service.TransformationException;
@@ -24,28 +25,24 @@ public class XsltTransformerTest {
     public void shouldTransformGivenFile() throws TransformationException, IOException, ParserConfigurationException, SAXException {
         URL xsltFile =  Resources.getResource("sample_xslt.xslt");
         byte[] fileToTransform = readFile("xmlForTesting.xml");
-        XsltTransformer t = new XsltTransformer();
-        StringWriter wr = t.transform(xsltFile.toString(),fileToTransform);
-        String resultXml = wr.toString();
-        //
+        StringWriter wr = XsltTransformer.transform(xsltFile.toString(),fileToTransform);
+        
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(new InputSource( new StringReader( wr.toString() ) ));
-        Assert.assertTrue(doc.getDocumentElement().getNodeName().equals("html"));
-        Assert.assertTrue(doc.getElementsByTagName("h2").getLength() == 1);
-        Assert.assertTrue(doc.getElementsByTagName("table").getLength() == 1);
-        Assert.assertTrue(doc.getElementsByTagName("th").getLength() == 2);
-        Assert.assertTrue(doc.getElementsByTagName("tr").getLength() == 26 + 1);
-        Assert.assertTrue(doc.getElementsByTagName("td").getLength() == 2 * 26);
-
+        Document doc = dBuilder.parse(new InputSource(new StringReader(wr.toString())));
+        Assert.assertEquals("html", doc.getDocumentElement().getNodeName());
+        Assert.assertEquals(1, doc.getElementsByTagName("h2").getLength());
+        Assert.assertEquals(1, doc.getElementsByTagName("table").getLength());
+        Assert.assertEquals(2, doc.getElementsByTagName("th").getLength());
+        Assert.assertEquals(26 + 1, doc.getElementsByTagName("tr").getLength());
+        Assert.assertEquals(2 * 26, doc.getElementsByTagName("td").getLength());
     }
 
     @Test
     public void shouldTransformGivenFileWithInjection() throws IOException, TransformationException {
         URL xsltFile =  Resources.getResource("inject_node.xslt");
         byte[] fileToTransform = readFile("xmlForTestingParamInjection.xml");
-        XsltTransformer t = new XsltTransformer();
-        StringWriter wr = t.transform(xsltFile.toString(), fileToTransform, "sample");
+        StringWriter wr = XsltTransformer.transform(xsltFile.toString(), fileToTransform, "sample");
         String resultXml = wr.toString();
         Assert.assertTrue(resultXml.contains("<injected_node>"));
         Assert.assertTrue(resultXml.contains("sample"));
@@ -56,16 +53,13 @@ public class XsltTransformerTest {
     public void shouldFailForMalformedFile() throws IOException, TransformationException {
         URL xsltFile =  Resources.getResource("inject_node.xslt");
         byte[] fileToTransform = readFile("malformedFile.xml");
-        XsltTransformer t = new XsltTransformer();
-        StringWriter wr = t.transform(xsltFile.toString(), fileToTransform, "sample");
+        XsltTransformer.transform(xsltFile.toString(), fileToTransform, "sample");
     }
 
     private byte[] readFile(String fileName) throws IOException {
-        String myXml = IOUtils.toString(getClass().getResource(fileName),
-                Charsets.UTF_8);
+        String myXml = IOUtils.toString(getClass().getClassLoader().getResource(fileName), Charsets.UTF_8);
         byte[] bytes = myXml.getBytes("UTF-8");
         InputStream contentStream = new ByteArrayInputStream(bytes);
         return IOUtils.toByteArray(contentStream);
-
     }
 }
