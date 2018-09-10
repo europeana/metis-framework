c17f62cecf1ed0daf970c8853c4a2cfe57b2a582
stzanakis <simon.tzanakis@europeana.eu>
2018-09-10 15:15:42 +0200

    MET-1175 Fix correct encoding of '#' character in url for enrichment.

--- metis-enrichment/metis-enrichment-client/src/main/java/eu/europeana/enrichment/rest/client/EnrichmentClient.java
+++ metis-enrichment/metis-enrichment-client/src/main/java/eu/europeana/enrichment/rest/client/EnrichmentClient.java
@@ -3,6 +3,13 @@ package eu.europeana.enrichment.rest.client;
 import static eu.europeana.metis.RestEndpoints.ENRICHMENT_BYURI;
 import static eu.europeana.metis.RestEndpoints.ENRICHMENT_ENRICH;
 
+import eu.europeana.enrichment.api.exceptions.UnknownException;
+import eu.europeana.enrichment.api.external.InputValueList;
+import eu.europeana.enrichment.api.external.model.EnrichmentBase;
+import eu.europeana.enrichment.api.external.model.EnrichmentResultList;
+import eu.europeana.enrichment.utils.InputValue;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,15 +20,8 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import eu.europeana.enrichment.api.exceptions.UnknownException;
-import eu.europeana.enrichment.api.external.InputValueList;
-import eu.europeana.enrichment.api.external.model.EnrichmentBase;
-import eu.europeana.enrichment.api.external.model.EnrichmentResultList;
-import eu.europeana.enrichment.utils.InputValue;
-
 /**
- * REST API wrapper class abstracting the REST calls and providing a clean POJO
- * implementation
+ * REST API wrapper class abstracting the REST calls and providing a clean POJO implementation
  *
  * @author Yorgos.Mamakis@ europeana.eu
  */
@@ -32,6 +32,7 @@ public class EnrichmentClient {
 
   /**
    * Constructor with required endpoint prefix.
+   *
    * @param endpoint the endpoint of the rest api
    */
   public EnrichmentClient(String endpoint) {
@@ -49,7 +50,8 @@ public class EnrichmentClient {
     inList.setInputValues(values);
 
     try {
-      return template.postForObject(endpoint + ENRICHMENT_ENRICH, inList, EnrichmentResultList.class);
+      return template
+          .postForObject(endpoint + ENRICHMENT_ENRICH, inList, EnrichmentResultList.class);
     } catch (RestClientException e) {
       throw new UnknownException("Enrichment client call failed.", e);
     }
@@ -62,8 +64,17 @@ public class EnrichmentClient {
    * @return the enriched information
    */
   public EnrichmentBase getByUri(String uri) {
-    UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(endpoint + ENRICHMENT_BYURI)
-        .queryParam("uri", uri);
+
+    String encodedUri;
+    try {
+      encodedUri = URLEncoder.encode(uri, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      throw new IllegalStateException(e);
+    }
+
+    UriComponentsBuilder builder;
+    builder = UriComponentsBuilder.fromHttpUrl(endpoint + ENRICHMENT_BYURI)
+        .queryParam("uri", encodedUri);
 
     HttpHeaders headers = new HttpHeaders();
     headers.set("Content-Type", MediaType.APPLICATION_XML_VALUE);
