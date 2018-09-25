25677d302141424f6ef303eafd00ea43d598f93c
jochen-vermeulen <jochen.vermeulen@europeana.eu>
2018-09-25 12:49:13 +0200

    MET-1416: Output the correct string value for the type.

--- metis-indexing/src/main/java/eu/europeana/indexing/solr/property/ProxySolrCreator.java
+++ metis-indexing/src/main/java/eu/europeana/indexing/solr/property/ProxySolrCreator.java
@@ -41,7 +41,7 @@ public class ProxySolrCreator implements PropertySolrCreator<Proxy> {
     SolrPropertyUtils.addValues(doc, EdmLabel.PROXY_EDM_HAS_MET, proxy.getEdmHasMet());
     SolrPropertyUtils.addValues(doc, EdmLabel.PROXY_EDM_ISRELATEDTO, proxy.getEdmIsRelatedTo());
     if (proxy.getEdmType() != null) {
-      doc.addField(EdmLabel.PROVIDER_EDM_TYPE.toString(), proxy.getEdmType());
+      doc.addField(EdmLabel.PROVIDER_EDM_TYPE.toString(), proxy.getEdmType().getEnumNameValue());
     }
   }
 }
