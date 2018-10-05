a01420454ab61cd0d7ae5bc903205183638622ca
stzanakis <simon.tzanakis@europeana.eu>
2018-10-05 14:43:42 +0200

    MET-1404 Set IsShownAt is part of the urlTypes to be used for extracting medatada.

--- metis-media-service/src/main/java/eu/europeana/metis/mediaservice/UrlType.java
+++ metis-media-service/src/main/java/eu/europeana/metis/mediaservice/UrlType.java
@@ -3,10 +3,11 @@ package eu.europeana.metis.mediaservice;
 import java.util.Collection;
 
 public enum UrlType {
-  
+
   OBJECT, HAS_VIEW, IS_SHOWN_BY, IS_SHOWN_AT;
 
   static boolean shouldExtractMetadata(Collection<UrlType> resourceTypes) {
-    return resourceTypes.stream().anyMatch(t -> t == UrlType.HAS_VIEW || t == UrlType.IS_SHOWN_BY);
+    return resourceTypes.stream().anyMatch(
+        t -> t == UrlType.HAS_VIEW || t == UrlType.IS_SHOWN_BY || t == UrlType.IS_SHOWN_AT);
   }
 }
