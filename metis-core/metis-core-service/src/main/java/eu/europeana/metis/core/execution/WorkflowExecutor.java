07b4fbb5b29c5ca6e9710b7e9e32820c20375be0
stzanakis <simon.tzanakis@europeana.eu>
2019-02-06 16:20:20 +0100

    MET-1712 Update workflow execution after it was identified as cancelled
    This is required so that the cancelling information will not be lost during the last workflow execution update in the database.

--- metis-core/metis-core-service/src/main/java/eu/europeana/metis/core/execution/WorkflowExecutor.java
+++ metis-core/metis-core-service/src/main/java/eu/europeana/metis/core/execution/WorkflowExecutor.java
@@ -89,6 +89,8 @@ public class WorkflowExecutor implements Callable<WorkflowExecution> {
 
     // Process the results
     if (finishDate == null && workflowExecutionDao.isCancelling(workflowExecution.getId())) {
+      // Update workflowExecution first, to retrieve cancelling information from db
+      workflowExecution = workflowExecutionDao.getById(workflowExecution.getId().toString());
       // If the workflow was cancelled before it had the chance to finish, we cancel all remaining
       // plugins.
       workflowExecution.setAllRunningAndInqueuePluginsToCancelled();
