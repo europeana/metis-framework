f3626a71a273b5e2fef4c5ff967d2b3a0989f0e4
jochen-vermeulen <jochen.vermeulen@europeana.eu>
2019-06-21 11:08:53 +0200

    MET-1988: Process review comments.

--- metis-indexing/src/main/java/eu/europeana/indexing/tiers/model/MetadataTier.java
+++ metis-indexing/src/main/java/eu/europeana/indexing/tiers/model/MetadataTier.java
@@ -5,19 +5,12 @@ package eu.europeana.indexing.tiers.model;
  */
 public enum MetadataTier implements Tier {
 
-  T0("0", 0), TA("A", 1), TB("B", 2), TC("C", 3);
+  T0(0), TA(1), TB(2), TC(3);
 
-  private final String label;
   private final int level;
 
-  MetadataTier(String label, int level) {
-    this.label = label;
+  MetadataTier(int level) {
     this.level = level;
-  }
-
-  @Override
-  public String getLabel() {
-    return label;
   }
 
   @Override
