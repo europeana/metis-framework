167913dd95aa4c8b5003a9ea89fcce73a3f92821
jochen-vermeulen <jochen.vermeulen@europeana.eu>
2019-09-10 16:28:57 +0200

    MET-1936: Unit tests for language tier classification.

--- metis-indexing/src/main/java/eu/europeana/indexing/tiers/metadata/LanguageTagStatistics.java
+++ metis-indexing/src/main/java/eu/europeana/indexing/tiers/metadata/LanguageTagStatistics.java
@@ -1,20 +1,35 @@
 package eu.europeana.indexing.tiers.metadata;
 
+import eu.europeana.corelib.definitions.jibx.AboutType;
 import eu.europeana.corelib.definitions.jibx.Concept;
+import eu.europeana.corelib.definitions.jibx.EuropeanaType.Choice;
 import eu.europeana.corelib.definitions.jibx.LiteralType;
 import eu.europeana.corelib.definitions.jibx.PlaceType;
 import eu.europeana.corelib.definitions.jibx.ResourceOrLiteralType;
 import eu.europeana.corelib.definitions.jibx.TimeSpanType;
-import eu.europeana.indexing.utils.RdfWrapper;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
-
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * This class is able to compute statistics on language usage in an entity. It counts for which of
+ * the defined property types a property is present that:
+ * <ol><li>
+ * has a language qualification itself, or,
+ * <li>refers to a contextual entity (edm:Place, edm:TimeSpan or skos:concept) that has a language
+ * qualification (in a skos:prefLabel).</li>
+ * </li></ol>
+ * It is then able to return for how many of the known property types a language qualification is
+ * present.
+ */
 class LanguageTagStatistics {
 
   enum PropertyType {
@@ -42,76 +57,230 @@ class LanguageTagStatistics {
     EDM_IS_RELATED_TO
   }
 
-  private final Set<String> contextualClassesWithValidLanguages = new HashSet<>();
-  private final EnumSet<PropertyType> propertiesInEntity = EnumSet.noneOf(PropertyType.class);
-  private final EnumSet<PropertyType> propertiesWithLanguage = EnumSet.noneOf(PropertyType.class);
+  private final Set<String> contextualClassesWithLanguage = new HashSet<>();
+  private final EnumSet<PropertyType> allAddedProperties = EnumSet.noneOf(PropertyType.class);
+  private final EnumSet<PropertyType> addedPropertiesWithLanguage = EnumSet
+      .noneOf(PropertyType.class);
 
-  LanguageTagStatistics(RdfWrapper entity) {
-    entity.getPlaces().stream().filter(this::hasValidLanguage).map(PlaceType::getAbout)
-        .forEach(contextualClassesWithValidLanguages::add);
-    entity.getTimeSpans().stream().filter(this::hasValidLanguage).map(TimeSpanType::getAbout)
-        .forEach(contextualClassesWithValidLanguages::add);
-    entity.getConcepts().stream().filter(this::hasValidLanguage).map(Concept::getAbout)
-        .forEach(contextualClassesWithValidLanguages::add);
+  /**
+   * Constructor.
+   *
+   * @param places The place objects that may be referenced. Can be null or empty, but can't contain
+   * null values.
+   * @param timeSpans The time span objects that may be referenced. Can be null or empty, but can't
+   * contain null values.
+   * @param concepts The concept objects that may be referenced. Can be null or empty, but can't
+   * contain null values.
+   */
+  LanguageTagStatistics(List<PlaceType> places, List<TimeSpanType> timeSpans,
+      List<Concept> concepts) {
+    getStream(places).filter(LanguageTagStatistics::hasValidLanguage).map(AboutType::getAbout)
+        .forEach(contextualClassesWithLanguage::add);
+    getStream(timeSpans).filter(LanguageTagStatistics::hasValidLanguage).map(AboutType::getAbout)
+        .forEach(contextualClassesWithLanguage::add);
+    getStream(concepts).filter(LanguageTagStatistics::hasValidLanguage).map(AboutType::getAbout)
+        .forEach(contextualClassesWithLanguage::add);
+  }
+
+  Set<String> getContextualClassesWithLanguage() {
+    return Collections.unmodifiableSet(contextualClassesWithLanguage);
+  }
+
+  Set<PropertyType> getAllAddedProperties() {
+    return Collections.unmodifiableSet(allAddedProperties);
+  }
+
+  Set<PropertyType> getAddedPropertiesWithLanguage() {
+    return Collections.unmodifiableSet(addedPropertiesWithLanguage);
+  }
+
+  boolean containsContextualClass(String about) {
+    return contextualClassesWithLanguage.contains(about);
   }
 
   private static <T> Stream<T> getStream(List<T> list) {
     return Optional.ofNullable(list).map(List::stream).orElseGet(Stream::empty);
   }
 
-  private boolean hasValidLanguage(PlaceType place) {
-    return getStream(place.getPrefLabelList()).anyMatch(this::hasValidLanguage);
+  private static boolean hasValidLanguage(PlaceType place) {
+    return getStream(place.getPrefLabelList()).anyMatch(LanguageTagStatistics::hasValidLanguage);
   }
 
-  private boolean hasValidLanguage(TimeSpanType timespan) {
-    return getStream(timespan.getPrefLabelList()).anyMatch(this::hasValidLanguage);
+  private static boolean hasValidLanguage(TimeSpanType timespan) {
+    return getStream(timespan.getPrefLabelList()).anyMatch(LanguageTagStatistics::hasValidLanguage);
   }
 
-  private boolean hasValidLanguage(Concept concept) {
+  private static boolean hasValidLanguage(Concept concept) {
     return getStream(concept.getChoiceList()).filter(Concept.Choice::ifPrefLabel)
-        .map(Concept.Choice::getPrefLabel).anyMatch(this::hasValidLanguage);
+        .map(Concept.Choice::getPrefLabel).anyMatch(LanguageTagStatistics::hasValidLanguage);
   }
 
-  private boolean hasValidLanguage(LiteralType literal) {
+  private static boolean hasValidLanguage(LiteralType literal) {
     return StringUtils.isNotBlank(literal.getString()) && literal.getLang() != null && StringUtils
         .isNotBlank(literal.getLang().getLang());
   }
 
+  /**
+   * Adds a property occurrence to the statistics.
+   *
+   * @param property The value of the property.
+   * @param type The type of the property.
+   */
   void addToStatistics(LiteralType property, PropertyType type) {
-    if (StringUtils.isNotBlank(property.getString())) {
-      // If the literal has a value, check whether the language is not empty.
-      propertiesInEntity.add(type);
-      if (property.getLang() != null && StringUtils.isNotBlank(property.getLang().getLang())) {
-        propertiesWithLanguage.add(type);
-      }
+    if (type == null) {
+      // Sanity check
+      throw new IllegalArgumentException();
     }
-  }
-
-  void addToStatistics(ResourceOrLiteralType property, PropertyType type) {
     if (property == null) {
       // sanity check.
       return;
     }
     if (StringUtils.isNotBlank(property.getString())) {
       // If the literal has a value, check whether the language is not empty.
-      propertiesInEntity.add(type);
+      allAddedProperties.add(type);
       if (property.getLang() != null && StringUtils.isNotBlank(property.getLang().getLang())) {
-        propertiesWithLanguage.add(type);
-      }
-    } else if (property.getResource() != null && StringUtils
-        .isNotBlank(property.getResource().getResource())) {
-      propertiesInEntity.add(type);
-      if (contextualClassesWithValidLanguages.contains(property.getResource().getResource())) {
-        propertiesWithLanguage.add(type);
+        addedPropertiesWithLanguage.add(type);
       }
     }
   }
 
+  /**
+   * Adds a property occurrence to the statistics.
+   *
+   * @param property The value of the property.
+   * @param type The type of the property.
+   */
+  void addToStatistics(ResourceOrLiteralType property, PropertyType type) {
+    if (type == null) {
+      // Sanity check
+      throw new IllegalArgumentException();
+    }
+    if (property == null) {
+      // sanity check.
+      return;
+    }
+    if (StringUtils.isNotBlank(property.getString())) {
+      // If the property has a value, check whether the language is not empty.
+      allAddedProperties.add(type);
+      if (property.getLang() != null && StringUtils.isNotBlank(property.getLang().getLang())) {
+        addedPropertiesWithLanguage.add(type);
+      }
+    }
+    if (property.getResource() != null && StringUtils
+        .isNotBlank(property.getResource().getResource())) {
+      // If the property has a resource link, check whether the link is a contextual class.
+      allAddedProperties.add(type);
+      if (containsContextualClass(property.getResource().getResource())) {
+        addedPropertiesWithLanguage.add(type);
+      }
+    }
+  }
+
+  /**
+   * Adds a property occurrence to the statistics.
+   *
+   * @param properties The values of the property.
+   * @param type The type of the property.
+   */
   void addToStatistics(List<? extends ResourceOrLiteralType> properties, PropertyType type) {
+    if (type == null) {
+      // Sanity check
+      throw new IllegalArgumentException();
+    }
+    if (properties == null) {
+      // sanity check.
+      return;
+    }
     getStream(properties).forEach(property -> addToStatistics(property, type));
   }
 
+  /**
+   * Adds a property occurrence to the statistics.
+   *
+   * @param choice The choice list containing the property value (s). The type of the property is
+   * determined based on which element of the choice is set.
+   */
+  void addToStatistics(Choice choice) {
+    if (choice == null) {
+      // sanity check.
+      return;
+    }
+    for (ProxyChoiceKind kind : ProxyChoiceKind.values()) {
+      kind.valueProcessing.accept(choice, this);
+    }
+  }
+
+  private enum ProxyChoiceKind {
+
+    DC_COVERAGE(Choice::ifCoverage, Choice::getCoverage, PropertyType.DC_COVERAGE),
+    DC_DESCRIPTION(Choice::ifDescription, Choice::getDescription, PropertyType.DC_DESCRIPTION),
+    DC_FORMAT(Choice::ifFormat, Choice::getFormat, PropertyType.DC_FORMAT),
+    DC_RELATION(Choice::ifRelation, Choice::getRelation, PropertyType.DC_RELATION),
+    DC_RIGHTS(Choice::ifRights, Choice::getRights, PropertyType.DC_RIGHTS),
+    DC_SOURCE(Choice::ifSource, Choice::getSource, PropertyType.DC_SOURCE),
+    DC_SUBJECT(Choice::ifSubject, Choice::getSubject, PropertyType.DC_SUBJECT),
+    DC_TITLE(Choice::ifTitle, Choice::getTitle, PropertyType.DC_TITLE,
+        LanguageTagStatistics::addToStatistics),
+    DC_TYPE(Choice::ifType, Choice::getType, PropertyType.DC_TYPE),
+    DCTERMS_ALTERNATIVE(Choice::ifAlternative, Choice::getAlternative,
+        PropertyType.DCTERMS_ALTERNATIVE, LanguageTagStatistics::addToStatistics),
+    DCTERMS_HAS_PART(Choice::ifHasPart, Choice::getHasPart, PropertyType.DCTERMS_HAS_PART),
+    DCTERMS_IS_PART_OF(Choice::ifIsPartOf, Choice::getIsPartOf, PropertyType.DCTERMS_IS_PART_OF),
+    DCTERMS_IS_REFERENCED_BY(Choice::ifIsReferencedBy, Choice::getIsReferencedBy,
+        PropertyType.DCTERMS_IS_REFERENCED_BY),
+    DCTERMS_MEDIUM(Choice::ifMedium, Choice::getMedium, PropertyType.DCTERMS_MEDIUM),
+    DCTERMS_PROVENANCE(Choice::ifProvenance, Choice::getProvenance,
+        PropertyType.DCTERMS_PROVENANCE),
+    DCTERMS_REFERENCES(Choice::ifReferences, Choice::getReferences,
+        PropertyType.DCTERMS_REFERENCES),
+    DCTERMS_SPATIAL(Choice::ifSpatial, Choice::getSpatial, PropertyType.DCTERMS_SPATIAL),
+    DCTERMS_TABLE_OF_CONTENTS(Choice::ifTableOfContents, Choice::getTableOfContents,
+        PropertyType.DCTERMS_TABLE_OF_CONTENTS),
+    DCTERMS_TEMPORAL(Choice::ifTemporal, Choice::getTemporal, PropertyType.DCTERMS_TEMPORAL);
+
+    private final BiConsumer<Choice, LanguageTagStatistics> valueProcessing;
+
+    ProxyChoiceKind(Predicate<Choice> choiceSelection,
+        Function<Choice, ResourceOrLiteralType> valueExtraction, PropertyType type) {
+      this(choiceSelection, valueExtraction, type, LanguageTagStatistics::addToStatistics);
+    }
+
+    <T> ProxyChoiceKind(Predicate<Choice> choiceSelection, Function<Choice, T> valueExtraction,
+        PropertyType type, InclusionInStatistics<T> inclusionInStatistics) {
+      this.valueProcessing = (choice, statistics) -> {
+        if (choiceSelection.test(choice)) {
+          inclusionInStatistics
+              .includeInStatistics(statistics, valueExtraction.apply(choice), type);
+        }
+      };
+    }
+
+    @FunctionalInterface
+    interface InclusionInStatistics<T> {
+
+      /**
+       * Adds the value to the statistics.
+       *
+       * @param stats The statistics to which to add the value.
+       * @param value The value to add.
+       * @param type The type of the value to add.
+       */
+      void includeInStatistics(LanguageTagStatistics stats, T value, PropertyType type);
+    }
+  }
+
+  /**
+   * Computes for how many of the known property types a language qualification is present. This is
+   * returned as a ratio (i.e. a fraction of the total number of known property types). If no
+   * properties were added, this method returns 0.
+   *
+   * @return The ratio.
+   */
   double getPropertyWithLanguageRatio() {
-    return ((double) propertiesWithLanguage.size()) / propertiesInEntity.size();
+    final Set<PropertyType> addedProperties = getAllAddedProperties();
+    if (addedProperties.isEmpty()) {
+      return 0;
+    }
+    return ((double) getAddedPropertiesWithLanguage().size()) / addedProperties.size();
   }
 }
