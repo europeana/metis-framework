88db2455f7642bc7efafc6d0482b475214fb936a
jochen-vermeulen <jochen.vermeulen@europeana.eu>
2020-01-21 17:25:24 +0100

    MET-2431: Fix compile issue in previous commit.

--- metis-dereference/metis-dereference-service/src/main/java/eu/europeana/metis/dereference/service/utils/VocabularyCandidates.java
+++ metis-dereference/metis-dereference-service/src/main/java/eu/europeana/metis/dereference/service/utils/VocabularyCandidates.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import eu.europeana.metis.dereference.Vocabulary;
