d1101ed27ad80e14b1b8b038646d52eef1b17ad3
stzanakis <simon.tzanakis@europeana.eu>
2020-05-15 09:49:39 +0200

    MET-2678 Update list of embeddable media urls and provide pattern matching

--- metis-indexing/src/main/java/eu/europeana/indexing/tiers/media/EmbeddableMedia.java
+++ metis-indexing/src/main/java/eu/europeana/indexing/tiers/media/EmbeddableMedia.java
@@ -5,29 +5,53 @@ import eu.europeana.indexing.utils.WebResourceLinkType;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.EnumSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * This class determines whether a given web resource represents embeddable media.
  */
 final class EmbeddableMedia {
 
-  private static final Collection<String> PREFIXES = Arrays.asList(
-      "urn:soundcloud:"
-      , "http://player.vimeo.com/video/"
-      , "https://vimeo.com/"
-      , "https://sketchfab.com/models/"
-      , "http://sounds.bl.uk/embed/"
-      , "http://eusounds.ait.co.at/"
-      , "http://images3.noterik.com/edna/domain/euscreenxl/"
-
-      , "http://www.ccma.cat/tv3/alacarta/programa/titol/video/"
-      , "http://www.ina.fr/video/"
-      , "http://www.ina.fr/*/video/"
-      , "http://api.picturepipe.net/api/html/widgets/public/playout_cloudfront?token="
-      , "https://api.picturepipe.net/api/html/widgets/public/playout_cloudfront?token="
-      , "http://archives.crem-cnrs.fr/archives/items/"
-      , "http://www.theeuropeanlibrary.org/tel4/newspapers/issue/fullscreen/"
+  private static final Collection<String> URL_MATCHING_LIST = Arrays.asList(
+      //SoundCould
+      "http://soundcloud.com/",
+      "https://soundcloud.com/",
+      //Vimeo
+      "http://player.vimeo.com/video/",
+      "https://player.vimeo.com/video/",
+      "https://vimeo.com/",
+      "http://vimeo.com/",
+      //YouTube
+      "https://*.youtube.com/watch",
+      "https://*.youtube.com/v/",
+      "https://youtu.be/",
+      //SketchFab
+      "https://sketchfab.com/models/",
+      "https://skfb.ly/",
+      //Dismarc
+      "http://www.dismarc.org/player/",
+      "http://eusounds.ait.co.at/player/",
+      //Europeana
+      "http://www.ccma.cat/tv3/alacarta/programa/titol/video/",
+      "http://www.ina.fr/video/",
+      "http://www.ina.fr/*/video/",
+      "http://api.picturepipe.net/api/html/widgets/public/playout_cloudfront?token=",
+      "https://api.picturepipe.net/api/html/widgets/public/playout_cloudfront?token=",
+      "http://archives.crem-cnrs.fr/archives/items/",
+      "http://www.theeuropeanlibrary.org/tel4/newspapers/issue/fullscreen/",
+      //BritishLibrary
+      "http://sounds.bl.uk/embed/",
+      //EUScreen
+      "http://www.euscreen.eu/item.html"
   );
+
+  // Create patterns, update wildcards to proper format and add a wildcard at the end of each url
+  private static final Collection<Pattern> PATTERNS = URL_MATCHING_LIST.stream()
+      .map(prefix -> prefix.replace("*", ".*"))
+      .map(prefix -> prefix.concat(".*")).map(Pattern::compile)
+      .collect(Collectors.toList());
 
   private EmbeddableMedia() {
   }
@@ -44,6 +68,6 @@ final class EmbeddableMedia {
   }
 
   private static boolean isEmbeddableMedia(String url) {
-    return PREFIXES.stream().anyMatch(url::startsWith);
+    return PATTERNS.stream().map(pattern -> pattern.matcher(url)).anyMatch(Matcher::matches);
   }
 }
