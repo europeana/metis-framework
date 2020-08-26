142e1d5069774fd6175d017395302a6e76e98c6b
JoanaCMS <joana.sousa@europeana.eu>
2020-08-26 15:32:33 +0200

    MET-657: Created new enum class representing IRI Violations

---
+++ metis-normalization/src/main/java/eu/europeana/normalization/IRIViolations.java
@@ -0,0 +1,19 @@
+package eu.europeana.normalization;
+
+/**
+ * This class lists the IRI Violations that are handled
+ *
+ * @author joana
+ */
+
+public enum IRIViolations {
+
+    UNWISE_CHARACTER,
+    WHITESPACE,
+    ILLEGAL_CHARACTER,
+    NOT_XML_SCHEMA_WHITESPACE,
+    DOUBLE_WHITESPACE,
+    CONTROL_CHARACTER,
+    COMPATIBILITY_CHARACTER
+
+}
