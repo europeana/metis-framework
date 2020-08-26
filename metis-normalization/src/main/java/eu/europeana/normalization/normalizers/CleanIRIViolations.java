e1a67102549f14625e2aa28ec38b9a08cb6bfb4a
Joana Sousa <joana.sousa@europeana.eu>
2020-08-26 15:46:50 +0200

    MET-657: Created new normalizer to clean IRI violations

---
+++ metis-normalization/src/main/java/eu/europeana/normalization/normalizers/CleanIRIViolations.java
@@ -0,0 +1,11 @@
+package eu.europeana.normalization.normalizers;
+
+import java.util.List;
+
+public class CleanIRIViolations extends EdmValueNormalizer{
+
+    @Override
+    public List<NormalizedValueWithConfidence> normalizeValue(String value) {
+        return null;
+    }
+}
