67ddbcce77b1f7a5a6b998fa87729cd16540caea
Joana Sousa <joana.sousa@europeana.eu>
2020-09-02 16:47:48 +0200

    MET-657: Started creating unit tests for CleanIRIViolationsNormalizer

---
+++ metis-normalization/src/test/java/eu/europeana/normalization/normalizers/CleanIRIViolationsNormalizerTest.java
@@ -0,0 +1,21 @@
+package eu.europeana.normalization.normalizers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CleanIRIViolationsNormalizerTest {
+
+    private static boolean setUpIsDone = false;
+
+    private final CleanIRIViolationsNormalizer normalizer = new CleanIRIViolationsNormalizer();
+
+    @BeforeEach
+    public void setUp(){
+
+    }
+
+    @Test
+    public void whitespaceIRIViolationCleanupTest(){
+
+    }
+}
