1cdbdc0d561b9bef4c63c452f00b1553295e0e7c
Joana Sousa <joana.sousa@europeana.eu>
2020-09-02 15:36:48 +0200

    MET-657: Added more queries for the CleanIRIViolationsNormalizer
    Corrected some of the existing queries in the normalizer

--- metis-normalization/src/main/java/eu/europeana/normalization/normalizers/CleanIRIViolationsNormalizer.java
+++ metis-normalization/src/main/java/eu/europeana/normalization/normalizers/CleanIRIViolationsNormalizer.java
@@ -9,14 +9,38 @@ import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This normalizer cleans, or 'escapes', media link values, replacing all IRI Violation characters
+ * with their respective encoding character.
+ *
+ * @author Joana Sousa
+ */
+
 public class CleanIRIViolationsNormalizer implements ValueNormalizeAction{
 
     private static final Namespace.Element RDF_RESOURCE = Namespace.RDF.getElement("resource");
-    private static final Namespace.Element EDM_IS_SHOWN_BY = Namespace.EDM.getElement("isShownBy");
+    private static final Namespace.Element RDF_ABOUT = Namespace.RDF.getElement("about");
     private static final Namespace.Element ORE_AGGREGATION = Namespace.ORE.getElement("Aggregation");
+    private static final Namespace.Element EDM_IS_SHOWN_BY = Namespace.EDM.getElement("isShownBy");
+    private static final Namespace.Element EDM_IS_SHOWN_AT = Namespace.EDM.getElement("isShownAt");
+    private static final Namespace.Element EDM_OBJECT = Namespace.EDM.getElement("object");
+    private static final Namespace.Element EDM_HAS_VIEW = Namespace.EDM.getElement("hasView");
+    private static final Namespace.Element EDM_WEB_RESOURCE = Namespace.EDM.getElement("WebResource");
 
-    private static final XpathQuery RESOURCE_DATA_QUERY = new XpathQuery(
-            "/%s/%s/%s//@%s", XpathQuery.RDF_TAG, ORE_AGGREGATION, EDM_IS_SHOWN_BY, RDF_RESOURCE);
+    private static final XpathQuery RESOURCE_IS_SHOWN_BY_QUERY = new XpathQuery(
+            "/%s/%s/%s/@%s", XpathQuery.RDF_TAG, ORE_AGGREGATION, EDM_IS_SHOWN_BY, RDF_RESOURCE);
+
+    private static final XpathQuery RESOURCE_HAS_VIEW_QUERY = new XpathQuery(
+            "/%s/%s/%s/@%s", XpathQuery.RDF_TAG, ORE_AGGREGATION, EDM_HAS_VIEW,  RDF_RESOURCE);
+
+    private static final XpathQuery RESOURCE_IS_SHOWN_AT_QUERY = new XpathQuery(
+            "/%s/%s/%s/@%s", XpathQuery.RDF_TAG, ORE_AGGREGATION, EDM_IS_SHOWN_AT,  RDF_RESOURCE);
+
+    private static final XpathQuery RESOURCE_OBJECT_QUERY = new XpathQuery(
+            "/%s/%s/%s/@%s", XpathQuery.RDF_TAG, ORE_AGGREGATION, EDM_OBJECT,  RDF_RESOURCE);
+
+    private static final XpathQuery WEB_RESOURCE_ABOUT_QUERY = new XpathQuery(
+            "/%s/%s/@%s", XpathQuery.RDF_TAG, EDM_WEB_RESOURCE, RDF_ABOUT);
 
     private final IRIFactory iriFactory;
 
@@ -29,7 +53,8 @@ public class CleanIRIViolationsNormalizer implements ValueNormalizeAction{
 
     @Override
     public RecordNormalizeAction getAsRecordNormalizer() {
-        return new ValueNormalizeActionWrapper(this, RESOURCE_DATA_QUERY);
+        return new ValueNormalizeActionWrapper(this, RESOURCE_IS_SHOWN_BY_QUERY, RESOURCE_HAS_VIEW_QUERY,
+                RESOURCE_IS_SHOWN_AT_QUERY, RESOURCE_OBJECT_QUERY, WEB_RESOURCE_ABOUT_QUERY);
     }
 
     @Override
@@ -45,6 +70,8 @@ public class CleanIRIViolationsNormalizer implements ValueNormalizeAction{
             normalizedValue = iri.toURI().toString();
         } catch (URISyntaxException e) {
             e.printStackTrace();
+            //TODO: Add LOG (debug)
+            //TODO: Return empty list
         }
 
         return Collections.singletonList(new NormalizedValueWithConfidence(normalizedValue, 1));
