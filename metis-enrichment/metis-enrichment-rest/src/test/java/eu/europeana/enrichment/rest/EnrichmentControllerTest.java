ff2a94538d2f2437629196709732994d92fccfab
Joana Sousa <joana.sousa@europeana.eu>
2020-12-04 15:38:37 +0100

    MET-3127: Clean-up

--- metis-enrichment/metis-enrichment-rest/src/test/java/eu/europeana/enrichment/rest/EnrichmentControllerTest.java
+++ metis-enrichment/metis-enrichment-rest/src/test/java/eu/europeana/enrichment/rest/EnrichmentControllerTest.java
@@ -57,7 +57,7 @@ public class EnrichmentControllerTest {
     String uri = "http://www.example.com";
     Agent agent = getAgent(uri);
     ReferenceValue reference = new ReferenceValue(uri, anySet());
-    when(enrichmentServiceMock.enrichByEquivalenceValues(reference)).thenReturn(List.of(agent)); //TODO: Fix this
+    when(enrichmentServiceMock.enrichByEquivalenceValues(reference)).thenReturn(List.of(agent));
     enrichmentControllerMock.perform(get("/enrich/entity/equivalence")
         .param("uri", "http://www.example.com")
         .accept(MediaType.APPLICATION_JSON))
@@ -74,7 +74,7 @@ public class EnrichmentControllerTest {
     String uri = "http://www.example.com";
     Agent agent = getAgent(uri);
     ReferenceValue reference = new ReferenceValue(uri, anySet());
-    when(enrichmentServiceMock.enrichByEquivalenceValues(reference)).thenReturn(List.of(agent)); //TODO: Fix this
+    when(enrichmentServiceMock.enrichByEquivalenceValues(reference)).thenReturn(List.of(agent));
     Map<String, String> namespaceMap = getNamespaceMap();
     enrichmentControllerMock.perform(get("/enrich/entity/equivalence")
         .param("uri", "http://www.example.com")
