de01927ce3664592f1ceeea74d7645c02ed780b4
Joana Sousa <joana.sousa@europeana.eu>
2021-01-19 17:19:48 +0100

    MET-3303: Started writting unit tests in EnrichmentUtilsTest

--- metis-enrichment/metis-enrichment-client/src/test/java/eu/europeana/enrichment/utils/EnrichmentUtilsTest.java
+++ metis-enrichment/metis-enrichment-client/src/test/java/eu/europeana/enrichment/utils/EnrichmentUtilsTest.java
@@ -1,8 +1,24 @@
 package eu.europeana.enrichment.utils;
 
+import eu.europeana.metis.schema.convert.RdfConversionUtils;
+import eu.europeana.metis.schema.convert.SerializationException;
+import eu.europeana.metis.schema.jibx.RDF;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class EnrichmentUtilsTest {
+
+  private static RDF testRdf;
+
+  @BeforeAll
+  static void setUp() throws FileNotFoundException, SerializationException {
+
+    testRdf = RdfConversionUtils.convertInputStreamToRdf(
+        new FileInputStream("src/test/resources/sample_enrichment.xml"));
+
+  }
 
   @Test
   void testSetAdditionalData(){
