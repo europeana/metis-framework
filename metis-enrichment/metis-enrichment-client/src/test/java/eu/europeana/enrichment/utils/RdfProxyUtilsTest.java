1aa56784ee00dbb6658b5d27c14dc1a577bf67d2
Joana Sousa <joana.sousa@europeana.eu>
2021-01-26 16:56:02 +0100

    MET-3303: Starting the unit tests for RdfProxyUtilsTest

--- metis-enrichment/metis-enrichment-client/src/test/java/eu/europeana/enrichment/utils/RdfProxyUtilsTest.java
+++ metis-enrichment/metis-enrichment-client/src/test/java/eu/europeana/enrichment/utils/RdfProxyUtilsTest.java
@@ -1,5 +1,9 @@
 package eu.europeana.enrichment.utils;
 
+import eu.europeana.enrichment.api.internal.FieldType;
+import eu.europeana.metis.schema.jibx.RDF;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 public class RdfProxyUtilsTest {
@@ -7,6 +11,16 @@ public class RdfProxyUtilsTest {
 
   @Test
   void testAppendLinkToEuropeanaProxy(){
+
+    RDF rdf = new RDF(); //TODO: add proxy lists
+    //TODO: add europeana proxy
+    String link = "http://dummylink.com";
+    Set<FieldType> linkTypes = new HashSet<>();
+
+  }
+
+  @Test
+  void testGetProviderProxy(){
 
   }
 }
