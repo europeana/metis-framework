0d4c039805b007eddb03b9628a61eeec9a16b541
Joana Sousa <joana.sousa@europeana.eu>
2021-03-24 16:23:45 +0100

    MET-3479: Changed the excpetion BadRequest to IllegalArgument

--- metis-enrichment/metis-enrichment-service/src/main/java/eu/europeana/enrichment/service/EnrichmentService.java
+++ metis-enrichment/metis-enrichment-service/src/main/java/eu/europeana/enrichment/service/EnrichmentService.java
@@ -24,7 +24,9 @@ import javax.ws.rs.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Contains functionality for accessing entities from the enrichment database using {@link
@@ -82,7 +84,7 @@ public class EnrichmentService {
       LOGGER.debug("There was a problem converting the input to ReferenceTermType");
       // TODO: 08/02/2021 Maybe change the thrown exception to another type since this is an internal service and not a rest application,
       //  which also affect the required dependencies during unit tests. Then the dependency org.glassfish.jersey.core:jersey-common can be removed
-      throw new BadRequestException("The input values are invalid", e);
+      throw new IllegalArgumentException("The input values are invalid", e);
     }
   }
 
@@ -99,7 +101,7 @@ public class EnrichmentService {
       return persistentEntityResolver.resolveById(Set.of(referenceTerm)).get(referenceTerm);
     } catch (MalformedURLException e) {
       LOGGER.debug("There was a problem converting the input to ReferenceTermType");
-      throw new BadRequestException("The input values are invalid", e);
+      throw new IllegalArgumentException("The input values are invalid", e);
     }
   }
 
