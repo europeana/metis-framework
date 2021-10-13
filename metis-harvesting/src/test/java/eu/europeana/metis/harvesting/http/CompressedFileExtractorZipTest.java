cad20ae3515024d3ea8a2251a875d0f08f109a88
Adolfo Peixinho <adolfo.peixinho@europeana.eu>
2021-10-13 18:17:40 +0200

    met-3953 Forgot to replace the last slash /

--- metis-harvesting/src/test/java/eu/europeana/metis/harvesting/http/CompressedFileExtractorZipTest.java
+++ metis-harvesting/src/test/java/eu/europeana/metis/harvesting/http/CompressedFileExtractorZipTest.java
@@ -13,7 +13,8 @@ import org.junit.jupiter.api.Test;
 
 public class CompressedFileExtractorZipTest {
 
-  private final static String DESTINATION_DIR = "src"+File.separator+"test"+File.separator+"resources"+File.separator+"__files/";
+  private final static String DESTINATION_DIR = 
+          "src"+File.separator+"test"+File.separator+"resources"+File.separator+"__files"+File.separator;
   private final static int XML_FILES_COUNT = 13;
   private final static String FILE_NAME = "zipFileWithNestedZipFiles";
   private final static String FILE_NAME2 = "zipFileWithNestedFolders";
