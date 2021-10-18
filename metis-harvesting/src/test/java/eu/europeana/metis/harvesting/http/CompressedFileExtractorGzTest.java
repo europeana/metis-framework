831b75cac45b7401467ac8bf8dfceeb10bb19c48
Adolfo Peixinho <adolfo.peixinho@europeana.eu>
2021-10-18 09:46:19 +0200

    MET-3926 Added file extension static string

--- metis-harvesting/src/test/java/eu/europeana/metis/harvesting/http/CompressedFileExtractorGzTest.java
+++ metis-harvesting/src/test/java/eu/europeana/metis/harvesting/http/CompressedFileExtractorGzTest.java
@@ -1,6 +1,5 @@
 package eu.europeana.metis.harvesting.http;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -14,57 +13,54 @@ import org.junit.jupiter.api.Test;
 
 public class CompressedFileExtractorGzTest {
 
-  private final static String DESTINATION_DIR = String.format("src%1$stest%1$sresources%1$s__files%1$s", File.separator);
-  private final static int XML_FILES_COUNT = 13;
-  private final static String FILE_NAME = "gzFile";
-  private final static String FILE_NAME2 = "gzFileWithCompressedGZFiles";
-  private final static String FILE_NAME3 = "gzFilesWithMixedCompressedFiles";
-  private final static String XML_TYPE = "xml";
+    private final static String DESTINATION_DIR = String.format("src%1$stest%1$sresources%1$s__files%1$s", File.separator);
+    private final static int XML_FILES_COUNT = 13;
+    private final static String FILE_NAME = "gzFile";
+    private final static String FILE_NAME2 = "gzFileWithCompressedGZFiles";
+    private final static String FILE_NAME3 = "gzFilesWithMixedCompressedFiles";
+    private final static String XML_TYPE = "xml";
+    public static final String FILE_EXTENSION = ".tar.gz";
+    
+    @Test
+    public void shouldUnpackTheTarGzFilesRecursively() throws IOException {
+        CompressedFileExtractor.extractFile(Path.of(DESTINATION_DIR + FILE_NAME + FILE_EXTENSION),Path.of(DESTINATION_DIR));
+        Collection<File> files = getXMLFiles(DESTINATION_DIR + FILE_NAME);
+        assertNotNull(files);
+        assertEquals(XML_FILES_COUNT, files.size());
+    }
 
-  @Test
-  public void shouldUnpackTheTarGzFilesRecursively() throws IOException {
-    CompressedFileExtractor.extractFile(Path.of(DESTINATION_DIR + FILE_NAME + ".tar.gz"),
-            Path.of(DESTINATION_DIR));
-    Collection<File> files = getXMLFiles(DESTINATION_DIR + FILE_NAME);
-    assertNotNull(files);
-    assertEquals(XML_FILES_COUNT, files.size());
-  }
+    @Test
+    public void shouldUnpackTheTarGzFilesRecursivelyWithCompressedXMLFiles() throws IOException {
+        CompressedFileExtractor.extractFile(Path.of(DESTINATION_DIR + FILE_NAME2 + FILE_EXTENSION),Path.of(DESTINATION_DIR));
+        Collection<File> files = getXMLFiles(DESTINATION_DIR + FILE_NAME2);
+        assertNotNull(files);
+        assertEquals(XML_FILES_COUNT, files.size());
+    }
 
-  @Test
-  public void shouldUnpackTheTarGzFilesRecursivelyWithCompressedXMLFiles() throws IOException {
-    CompressedFileExtractor.extractFile(Path.of(DESTINATION_DIR + FILE_NAME2 + ".tar.gz"),
-            Path.of(DESTINATION_DIR));
-    Collection<File> files = getXMLFiles(DESTINATION_DIR + FILE_NAME2);
-    assertNotNull(files);
-    assertEquals(XML_FILES_COUNT, files.size());
-  }
+    @Test
+    public void shouldUnpackTheTGZFilesRecursivelyWithCompressedXMLFiles() throws IOException {
+        CompressedFileExtractor.extractFile(Path.of(DESTINATION_DIR + FILE_NAME2 + FILE_EXTENSION), Path.of(DESTINATION_DIR));
+        Collection<File> files = getXMLFiles(DESTINATION_DIR + FILE_NAME2);
+        assertNotNull(files);
+        assertEquals(XML_FILES_COUNT, files.size());
+    }
 
-  @Test
-  public void shouldUnpackTheTGZFilesRecursivelyWithCompressedXMLFiles() throws IOException {
-    CompressedFileExtractor
-            .extractFile(Path.of(DESTINATION_DIR + FILE_NAME2 + ".tgz"), Path.of(DESTINATION_DIR));
-    Collection<File> files = getXMLFiles(DESTINATION_DIR + FILE_NAME2);
-    assertNotNull(files);
-    assertEquals(XML_FILES_COUNT, files.size());
-  }
+    @Test
+    public void shouldUnpackTheTarGzFilesRecursivelyWithMixedNestedCompressedFiles() throws IOException {
+        CompressedFileExtractor.extractFile(Path.of(DESTINATION_DIR + FILE_NAME3 + FILE_EXTENSION), Path.of(DESTINATION_DIR));
+        Collection<File> files = getXMLFiles(DESTINATION_DIR + FILE_NAME3);
+        assertNotNull(files);
+        assertEquals(XML_FILES_COUNT, files.size());
+    }
 
-  @Test
-  public void shouldUnpackTheTarGzFilesRecursivelyWithMixedNestedCompressedFiles() throws IOException {
-    CompressedFileExtractor.extractFile(Path.of(DESTINATION_DIR + FILE_NAME3 + ".tar.gz"),
-            Path.of(DESTINATION_DIR));
-    Collection<File> files = getXMLFiles(DESTINATION_DIR + FILE_NAME3);
-    assertNotNull(files);
-    assertEquals(XML_FILES_COUNT, files.size());
-  }
+    private Collection<File> getXMLFiles(String folderLocation) {
+        return FileUtils.listFiles(new File(folderLocation), new String[]{XML_TYPE}, true);
+    }
 
-  private Collection<File> getXMLFiles(String folderLocation) {
-    return FileUtils.listFiles(new File(folderLocation), new String[]{XML_TYPE}, true);
-  }
-
-  @AfterAll
-  public static void cleanUp() throws IOException {
-    FileUtils.forceDelete(new File(DESTINATION_DIR + FILE_NAME));
-    FileUtils.forceDelete(new File(DESTINATION_DIR + FILE_NAME2));
-    FileUtils.forceDelete(new File(DESTINATION_DIR + FILE_NAME3));
-  }
+    @AfterAll
+    public static void cleanUp() throws IOException {
+        FileUtils.forceDelete(new File(DESTINATION_DIR + FILE_NAME));
+        FileUtils.forceDelete(new File(DESTINATION_DIR + FILE_NAME2));
+        FileUtils.forceDelete(new File(DESTINATION_DIR + FILE_NAME3));
+    }
 }
