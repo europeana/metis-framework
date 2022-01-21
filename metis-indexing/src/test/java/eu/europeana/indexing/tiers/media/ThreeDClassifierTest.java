4155e6f56c62bf479bdde88b1871a0136ea39fb0
Simon Tzanakis <Simon.Tzanakis@europeana.eu>
2022-01-21 17:49:28 +0100

    MET-4158 Code cleanup and tests

--- metis-indexing/src/test/java/eu/europeana/indexing/tiers/media/ThreeDClassifierTest.java
+++ metis-indexing/src/test/java/eu/europeana/indexing/tiers/media/ThreeDClassifierTest.java
@@ -1,6 +1,7 @@
 package eu.europeana.indexing.tiers.media;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -10,9 +11,14 @@ import static org.mockito.Mockito.spy;
 import eu.europeana.indexing.tiers.model.MediaTier;
 import eu.europeana.indexing.utils.RdfWrapper;
 import eu.europeana.indexing.utils.WebResourceWrapper;
+import eu.europeana.metis.schema.model.MediaType;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ThreeDClassifierTest {
 
@@ -42,28 +48,42 @@ class ThreeDClassifierTest {
   @Test
   void testClassifyEntityWithoutWebResources() {
     final RdfWrapper entity = mock(RdfWrapper.class);
-    doReturn(true).when(entity).hasThumbnails();
     assertEquals(MediaTier.T0, classifier.classifyEntityWithoutWebResources(entity, true));
     assertEquals(MediaTier.T0, classifier.classifyEntityWithoutWebResources(entity, false));
   }
 
-  /**
-   * This now depends only on the mime type.
-   */
-  @Test
-  void testClassifyWebResource() {
-    testClassifyWebResource("any mime type", MediaTier.T4);
-    testClassifyWebResource("   ", MediaTier.T0);
-    testClassifyWebResource("", MediaTier.T0);
-    testClassifyWebResource(null, MediaTier.T0);
+  private static Stream<Arguments> testClassifyWebResource() {
+    return Stream.of(
+        //If mime type is not blank
+        Arguments.of(MediaTier.T4, "any mime type"),
+        //If mime type is blank(only spaces)
+        Arguments.of(MediaTier.T0, "   "),
+        //If mime type is blank(empty string)
+        Arguments.of(MediaTier.T0, ""),
+        //If mime type is blank(null)
+        Arguments.of(MediaTier.T0, null)
+    );
   }
 
-  private void testClassifyWebResource(String mimeType, MediaTier expectedTier) {
+  @ParameterizedTest(name = "[{index}] - expectedTier:{0} for mimeType:{1}")
+  @MethodSource("testClassifyWebResource")
+  void testClassifyWebResource(MediaTier expectedTier, String mimeType) {
     final WebResourceWrapper webResource = mock(WebResourceWrapper.class);
     doReturn(mimeType).when(webResource).getMimeType();
+    //Any combination of hasLandingPage and hasEmbeddableMedia should not change the result
     assertEquals(expectedTier, classifier.classifyWebResource(webResource, false, false));
     assertEquals(expectedTier, classifier.classifyWebResource(webResource, true, false));
     assertEquals(expectedTier, classifier.classifyWebResource(webResource, false, true));
     assertEquals(expectedTier, classifier.classifyWebResource(webResource, true, true));
+  }
+
+  @Test
+  void extractResolutionTierMetadataTest() {
+    assertNotNull(classifier.extractResolutionTierMetadata(mock(WebResourceWrapper.class), MediaTier.T0));
+  }
+
+  @Test
+  void getMediaType() {
+    assertEquals(MediaType.THREE_D, classifier.getMediaType());
   }
 }
