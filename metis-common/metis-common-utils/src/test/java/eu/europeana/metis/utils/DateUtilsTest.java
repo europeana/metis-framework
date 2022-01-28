7ca2fe789ae6aac1be4930ae25472a298fd7b80c
Jorge Ortiz <jorge.ortiz@europeana.eu>
2022-01-28 17:56:27 +0100

    MET-4200 add unit tests for metis common utils

--- metis-common/metis-common-utils/src/test/java/eu/europeana/metis/utils/DateUtilsTest.java
+++ metis-common/metis-common-utils/src/test/java/eu/europeana/metis/utils/DateUtilsTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+/**
+ * Unit test for {@link DateUtils}
+ */
 class DateUtilsTest {
 
   private static Stream<Arguments> provideExpectedTimeAndTimeUnits() {
