90f02c2765df9165f6d4186629fafe6f1e8f76d8
Jorge Ortiz <jorge.ortiz@europeana.eu>
2022-02-08 18:22:31 +0100

    MET-4200 refactor unit test to run actual code

--- metis-common/metis-common-network/src/test/java/eu/europeana/metis/network/NetworkUtilTest.java
+++ metis-common/metis-common-network/src/test/java/eu/europeana/metis/network/NetworkUtilTest.java
@@ -2,9 +2,14 @@ package eu.europeana.metis.network;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocketFactory;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
@@ -24,12 +29,14 @@ class NetworkUtilTest {
   }
 
   @Test
-  void getAvailableLocalPortWithException() {
-    try (MockedStatic<NetworkUtil> networkUtilMockedStatic = mockStatic(NetworkUtil.class)) {
-      networkUtilMockedStatic.when(NetworkUtil::getAvailableLocalPort).thenThrow(IOException.class);
-      assertThrows(IOException.class, () -> {
-        NetworkUtil.getAvailableLocalPort();
-      });
+  void getAvailableLocalPortWithException() throws IOException {
+    final int BACKLOG = 100;
+    try (MockedStatic<SSLServerSocketFactory> sslServerSocketFactory = mockStatic(SSLServerSocketFactory.class)) {
+      ServerSocketFactory serverSocketFactory = mock(ServerSocketFactory.class);
+      sslServerSocketFactory.when(SSLServerSocketFactory::getDefault).thenReturn(serverSocketFactory);
+      when(serverSocketFactory.createServerSocket(0, BACKLOG, InetAddress.getByName("localhost"))).thenThrow(IOException.class);
+
+      assertThrows(IOException.class, () -> NetworkUtil.getAvailableLocalPort());
     }
   }
 }
