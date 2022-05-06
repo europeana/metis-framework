df961d34e29295ebc5c94da57d8c718d552e914a
Adolfo Peixinho <adolfo.peixinho@europeana.eu>
2022-05-06 18:51:44 +0200

    MET-2608 Improve dereference cache. Sonar still complains about code smell, missing param doc.

--- metis-dereference/metis-dereference-rest/src/main/java/eu/europeana/metis/dereference/rest/DereferencingManagementController.java
+++ metis-dereference/metis-dereference-rest/src/main/java/eu/europeana/metis/dereference/rest/DereferencingManagementController.java
@@ -77,6 +77,7 @@ public class DereferencingManagementController {
 
   /**
    * Empty the cache for a specific resource
+   * @param resourceId The resourceId to empty the cache for
   * */
   @PostMapping(value = RestEndpoints.CACHE_EMPTY_RESOURCE)
   @ResponseBody
@@ -88,6 +89,7 @@ public class DereferencingManagementController {
 
   /**
    * Empty the cache for a specific vocabulary, with all associated entities
+   * @param vocabularyId The vocabularyId to empty the cache for
    * */
   @PostMapping(value = RestEndpoints.CACHE_EMPTY_VOCABULARY)
   @ResponseBody
