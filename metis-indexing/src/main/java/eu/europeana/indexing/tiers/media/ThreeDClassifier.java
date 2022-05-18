cfbefed31e723b25df538cf54137544365056e93
JoanaCMS <70145179+JoanaCMS@users.noreply.github.com>
2022-05-18 08:33:18 +0200

    MET-4472 New Tier Calculation for 3D (#530)
    * MET-4472 Started implementing tier calculation for 3D
    
    * MET-4472 Fixed unit tests
    
    * MET-4472 Changes for tier 1 calculation
    
    * MET-4472 Changed code to remove bug from sonarqube
    
    * MET-4472 Trying to fix bug from sonarqube
    
    * MET-4472 Fixing unit tests and code
    
    * MET-4472 Added new unit test for null values
    
    * MET-4472 Code review changes
    
    Changed classification of webresource for 3D. Updated unit tests
    
    * MET-4472 Removed code smell

--- metis-indexing/src/main/java/eu/europeana/indexing/tiers/media/ThreeDClassifier.java
+++ metis-indexing/src/main/java/eu/europeana/indexing/tiers/media/ThreeDClassifier.java
@@ -4,9 +4,11 @@ import eu.europeana.indexing.tiers.model.MediaTier;
 import eu.europeana.indexing.tiers.view.ResolutionTierMetadata;
 import eu.europeana.indexing.tiers.view.ResolutionTierMetadata.ResolutionTierMetadataBuilder;
 import eu.europeana.indexing.utils.RdfWrapper;
+import eu.europeana.indexing.utils.WebResourceLinkType;
 import eu.europeana.indexing.utils.WebResourceWrapper;
 import eu.europeana.metis.schema.model.MediaType;
-import org.apache.commons.lang3.StringUtils;
+
+import java.util.Set;
 
 /**
  * Classifier for 3D objects.
@@ -30,8 +32,19 @@ class ThreeDClassifier extends AbstractMediaClassifier {
   @Override
   MediaTier classifyWebResource(WebResourceWrapper webResource, boolean hasLandingPage, boolean hasEmbeddableMedia) {
 
-    // T2-T4 if there is a mime type (any whatsoever), T0 otherwise.
-    return StringUtils.isNotBlank(webResource.getMimeType()) ? MediaTier.T4 : MediaTier.T0;
+    final MediaTier result;
+
+    if(webResource == null){
+      result = MediaTier.T0;
+    } else if(mimeTypeIsNotImageOrApplicationPdf(webResource) && containsIsShownByOrHasViewWebResource(webResource)){
+      result = MediaTier.T4;
+    } else if(hasLandingPage && onlyContainsShownAtWebResource(webResource)){
+      result = MediaTier.T1;
+    } else {
+      result = MediaTier.T0;
+    }
+
+    return result;
   }
 
   @Override
@@ -42,5 +55,23 @@ class ThreeDClassifier extends AbstractMediaClassifier {
   @Override
   MediaType getMediaType() {
     return MediaType.THREE_D;
+  }
+
+  private boolean mimeTypeIsNotImageOrApplicationPdf(WebResourceWrapper webResource){
+    String mimeType = webResource.getMimeType();
+    return mimeType != null && webResource.getMediaType() != MediaType.IMAGE && !mimeType.startsWith("application/pdf");
+  }
+
+  private boolean containsIsShownByOrHasViewWebResource(WebResourceWrapper webResource){
+    Set<WebResourceLinkType> extractedLinkTypes = webResource.getLinkTypes();
+    return extractedLinkTypes != null && (extractedLinkTypes.contains(WebResourceLinkType.IS_SHOWN_BY) ||
+            extractedLinkTypes.contains(WebResourceLinkType.HAS_VIEW));
+  }
+
+  private boolean onlyContainsShownAtWebResource(WebResourceWrapper webResource){
+    Set<WebResourceLinkType> linkTypes = webResource.getLinkTypes();
+    return linkTypes.contains(WebResourceLinkType.IS_SHOWN_AT) &&
+            !linkTypes.contains(WebResourceLinkType.IS_SHOWN_BY) &&
+            !linkTypes.contains(WebResourceLinkType.HAS_VIEW);
   }
 }
