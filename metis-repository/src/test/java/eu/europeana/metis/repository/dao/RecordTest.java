e8fb39d4414e7454cb8a9e9d672920162c2aa85c
Joana Sousa <joana.sousa@europeana.eu>
2022-05-19 15:05:05 +0200

    MET-4531 Removed code smell

--- metis-repository/src/test/java/eu/europeana/metis/repository/dao/RecordTest.java
+++ metis-repository/src/test/java/eu/europeana/metis/repository/dao/RecordTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class RecordTest {
+class RecordTest {
 
     private Record recordToTest;
     private final Instant instantUsedForTest = Instant.now();
