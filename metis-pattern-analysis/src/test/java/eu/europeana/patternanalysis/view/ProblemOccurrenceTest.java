f0234bf997fedbdc571596253a87954b280ba25b
Simon Tzanakis <simon.tzanakis@europeana.eu>
2022-05-20 12:14:40 +0200

    Feat/met 4458 implement p1 problem pattern (#537)
    * MET-4458 Introduce wrapper ProblemPatternAnalysis class
    
    * MET-4458 Centralize patterns titles
    
    * MET-4458 Compute static sets of global and non-global patterns
    
    * MET-4458 Add more unit tests
    
    * MET-4458 Fix sonar issues

--- metis-pattern-analysis/src/test/java/eu/europeana/patternanalysis/view/ProblemOccurrenceTest.java
+++ metis-pattern-analysis/src/test/java/eu/europeana/patternanalysis/view/ProblemOccurrenceTest.java
@@ -16,9 +16,12 @@ class ProblemOccurrenceTest {
     assertEquals("Duplicate titleA", problemOccurrence1.getMessageReport());
     assertTrue(CollectionUtils.isEqualCollection(List.of("recordId2", "recordId1"), problemOccurrence1.getAffectedRecordIds()));
 
-    final ProblemOccurrence problemOccurrence2 = new ProblemOccurrence("Duplicate titleB");
-    assertNotNull(problemOccurrence2.getAffectedRecordIds());
+    final ProblemOccurrence problemOccurrence2 = new ProblemOccurrence("Duplicate titleA", null);
+    assertEquals("Duplicate titleA", problemOccurrence2.getMessageReport());
     assertEquals(0, problemOccurrence2.getAffectedRecordIds().size());
-  }
 
+    final ProblemOccurrence problemOccurrence3 = new ProblemOccurrence("Duplicate titleB");
+    assertNotNull(problemOccurrence3.getAffectedRecordIds());
+    assertEquals(0, problemOccurrence3.getAffectedRecordIds().size());
+  }
 }
