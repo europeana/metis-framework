924d4cb907b24ffbfa8bf5b022999bfe96ce834f
Jorge Ortiz <jorge.ortiz@europeana.eu>
2022-05-23 17:34:05 +0200

    MET-2608 add DeferencingManagementController unit tests

--- metis-dereference/metis-dereference-rest/src/test/java/eu/europeana/metis/dereference/rest/DereferencingManagementControllerTest.java
+++ metis-dereference/metis-dereference-rest/src/test/java/eu/europeana/metis/dereference/rest/DereferencingManagementControllerTest.java
@@ -5,17 +5,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import eu.europeana.metis.dereference.Vocabulary;
 import eu.europeana.metis.dereference.rest.exceptions.RestResponseExceptionHandler;
 import eu.europeana.metis.dereference.service.DereferencingManagementService;
+import eu.europeana.metis.dereference.vocimport.exception.VocabularyImportException;
 import eu.europeana.metis.utils.RestEndpoints;
 import java.net.URL;
 import java.util.ArrayList;
@@ -28,21 +34,24 @@ import org.mockito.stubbing.Answer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+/**
+ * Unit tests {@link DereferencingController} class
+ */
 class DereferencingManagementControllerTest {
 
-  private DereferencingManagementService dereferencingManagementServiceMock;
-  private MockMvc dereferencingManagementControllerMock;
+  private DereferencingManagementService deRefManagementServiceMock;
+  private MockMvc deRefManagementControllerMock;
   private String testEmptyCacheResult = "";
 
   @BeforeEach
   void setUp() {
-    dereferencingManagementServiceMock = mock(DereferencingManagementService.class);
+    deRefManagementServiceMock = mock(DereferencingManagementService.class);
 
     DereferencingManagementController dereferencingManagementController = new DereferencingManagementController(
-        dereferencingManagementServiceMock, Set.of("valid.domain.com"));
+        deRefManagementServiceMock, Set.of("valid.domain.com"));
 
-    dereferencingManagementControllerMock = MockMvcBuilders.standaloneSetup(dereferencingManagementController)
-                                                           .setControllerAdvice(new RestResponseExceptionHandler()).build();
+    deRefManagementControllerMock = MockMvcBuilders.standaloneSetup(dereferencingManagementController)
+                                                   .setControllerAdvice(new RestResponseExceptionHandler()).build();
   }
 
   @Test
@@ -61,25 +70,27 @@ class DereferencingManagementControllerTest {
     dummyVocabList.add(dummyVocab1);
     dummyVocabList.add(dummyVocab2);
 
-    when(dereferencingManagementServiceMock.getAllVocabularies()).thenReturn(dummyVocabList);
+    when(deRefManagementServiceMock.getAllVocabularies()).thenReturn(dummyVocabList);
 
-    dereferencingManagementControllerMock.perform(get("/vocabularies"))
-                                         .andExpect(jsonPath("$[0].uris[0]", is("https://dummy1.org/path1")))
-                                         .andExpect(jsonPath("$[1].uris[0]", is("https://dummy2.org/path2")))
-                                         .andExpect(status().is(200));
+    deRefManagementControllerMock.perform(get("/vocabularies"))
+                                 .andExpect(jsonPath("$[0].uris[0]", is("https://dummy1.org/path1")))
+                                 .andExpect(jsonPath("$[1].uris[0]", is("https://dummy2.org/path2")))
+                                 .andExpect(status().is(200));
   }
 
   @Test
   void testLoadVocabularies_validDomain_expectSuccess() throws Exception {
-    doNothing().when(dereferencingManagementServiceMock).loadVocabularies(any(URL.class));
-    dereferencingManagementControllerMock.perform(
-        post("/load_vocabularies").param("directory_url", "https://valid.domain.com/test/call")).andExpect(status().is(200));
+    doNothing().when(deRefManagementServiceMock).loadVocabularies(any(URL.class));
+    deRefManagementControllerMock.perform(post("/load_vocabularies")
+                                     .param("directory_url", "https://valid.domain.com/test/call"))
+                                 .andExpect(status().is(200));
   }
 
   @Test
   void testLoadVocabularies_invalidDomain_expectFail() throws Exception {
-    dereferencingManagementControllerMock.perform(post("/load_vocabularies").param("directory_url", "https://invalid.domain.com"))
-                                         .andExpect(status().is(400));
+    deRefManagementControllerMock.perform(post("/load_vocabularies")
+                                     .param("directory_url", "https://invalid.domain.com"))
+                                 .andExpect(status().is(400));
   }
 
   @Test
@@ -87,9 +98,23 @@ class DereferencingManagementControllerTest {
     doAnswer((Answer<Void>) invocationOnMock -> {
       testEmptyCacheResult = "OK";
       return null;
-    }).when(dereferencingManagementServiceMock).emptyCache();
+    }).when(deRefManagementServiceMock).emptyCache();
 
-    dereferencingManagementControllerMock.perform(delete(RestEndpoints.CACHE_EMPTY)).andExpect(status().is(200));
+    deRefManagementControllerMock.perform(delete(RestEndpoints.CACHE_EMPTY))
+                                 .andExpect(status().is(200));
+
+    assertEquals("OK", testEmptyCacheResult);
+  }
+
+  @Test
+  void testEmptyCacheByEmptyXml() throws Exception {
+    doAnswer((Answer<Void>) invocationOnMock -> {
+      testEmptyCacheResult = "OK";
+      return null;
+    }).when(deRefManagementServiceMock).purgeByNullOrEmptyXml();
+
+    deRefManagementControllerMock.perform(delete(RestEndpoints.CACHE_EMPTY_XML))
+                                 .andExpect(status().is(200));
 
     assertEquals("OK", testEmptyCacheResult);
   }
@@ -113,10 +138,11 @@ class DereferencingManagementControllerTest {
     doAnswer((Answer<Void>) invocationOnMock -> {
       testEmptyCacheResult = "OK";
       return null;
-    }).when(dereferencingManagementServiceMock).purgeByResourceId(any(String.class));
+    }).when(deRefManagementServiceMock).purgeByResourceId(any(String.class));
 
-    dereferencingManagementControllerMock.perform(post("/cache/resource").param("resourceId", "12345"))
-                                         .andExpect(status().is(200));
+    deRefManagementControllerMock.perform(post("/cache/resource")
+                                     .param("resourceId", "12345"))
+                                 .andExpect(status().is(200));
 
     assertEquals("OK", testEmptyCacheResult);
   }
@@ -127,11 +153,44 @@ class DereferencingManagementControllerTest {
     doAnswer((Answer<Void>) invocationOnMock -> {
       testEmptyCacheResult = "OK";
       return null;
-    }).when(dereferencingManagementServiceMock).purgeByVocabularyId(any(String.class));
+    }).when(deRefManagementServiceMock).purgeByVocabularyId(any(String.class));
 
-    dereferencingManagementControllerMock.perform(post(RestEndpoints.CACHE_EMPTY_VOCABULARY).param("vocabularyId", "12345"))
-                                         .andExpect(status().is(200));
+    deRefManagementControllerMock.perform(post(RestEndpoints.CACHE_EMPTY_VOCABULARY)
+                                     .param("vocabularyId", "12345"))
+                                 .andExpect(status().is(200));
 
     assertEquals("OK", testEmptyCacheResult);
+  }
+
+  @Test
+  void testLoadVocabularies_expectBadRequest() throws Exception {
+    deRefManagementControllerMock.perform(post(RestEndpoints.LOAD_VOCABULARIES)
+                                     .param("directory_url", "directory"))
+                                 .andDo(print())
+                                 .andExpect(status().is(400))
+                                 .andExpect(content().string("The url of the directory to import is not valid."));
+  }
+
+  @Test
+  void testLoadVocabularies_expectBadContent() throws Exception {
+    doThrow(VocabularyImportException.class).when(deRefManagementServiceMock).loadVocabularies(any(URL.class));
+    deRefManagementControllerMock.perform(post(RestEndpoints.LOAD_VOCABULARIES)
+                                     .param("directory_url", "\\/tttp://test"))
+                                 .andDo(print())
+                                 .andExpect(status().is(400))
+                                 .andExpect(content().string("Provided directoryUrl '\\/tttp://test', failed to parse."));
+    verify(deRefManagementServiceMock, times(0)).loadVocabularies(any(URL.class));
+  }
+
+  @Test
+  void testLoadVocabularies_expectBadVocabulary() throws Exception {
+    doThrow(new VocabularyImportException("Cannot load vocabulary"))
+        .when(deRefManagementServiceMock).loadVocabularies(any(URL.class));
+    deRefManagementControllerMock.perform(post(RestEndpoints.LOAD_VOCABULARIES)
+                                     .param("directory_url", "https://valid.domain.com/test/call"))
+                                 .andDo(print())
+                                 .andExpect(status().is(502))
+                                 .andExpect(content().string("Cannot load vocabulary"));
+    verify(deRefManagementServiceMock, times(1)).loadVocabularies(any(URL.class));
   }
 }
