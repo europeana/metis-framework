0a9576007b68294c1d323d519a2d102ce4933ab3
Simon Tzanakis <Simon.Tzanakis@europeana.eu>
2022-10-27 11:35:55 +0200

    MET-4864 Add spring tomcat support

--- metis-authentication/metis-authentication-rest/src/main/java/eu/europeana/metis/authentication/rest/Application.java
+++ metis-authentication/metis-authentication-rest/src/main/java/eu/europeana/metis/authentication/rest/Application.java
@@ -1,13 +1,18 @@
 package eu.europeana.metis.authentication.rest;
 
+import java.util.Arrays;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 /**
  * The Spring boot application entry point.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
   /**
    * The main spring boot method.
@@ -16,5 +21,20 @@ public class Application {
    */
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return args -> {
+
+      System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+      String[] beanNames = ctx.getBeanDefinitionNames();
+      Arrays.sort(beanNames);
+      for (String beanName : beanNames) {
+        System.out.println(beanName);
+      }
+
+    };
   }
 }
