b9c29f06897e8574d5ae878718c1b1463491e763
Jorge Ortiz <jorge.ortiz@europeana.eu>
2022-12-07 16:26:03 +0100

    MET-2018_MET-4364 update java doc in ProcessedResult

--- metis-enrichment/metis-enrichment-client/src/main/java/eu/europeana/enrichment/rest/client/report/ProcessedResult.java
+++ metis-enrichment/metis-enrichment-client/src/main/java/eu/europeana/enrichment/rest/client/report/ProcessedResult.java
@@ -15,8 +15,8 @@ public class ProcessedResult<T> {
   private final RecordStatus recordStatus;
 
   /**
-   * Constructor with record
-   * @param processedRecord
+   * Constructor with record of type T
+   * @param processedRecord record that has been processed as result.
    */
   public ProcessedResult(T processedRecord) {
     this.processedRecord = processedRecord;
@@ -25,9 +25,9 @@ public class ProcessedResult<T> {
   }
 
   /**
-   * Constructor with record and report
-   * @param processedRecord
-   * @param reportMessages
+   * Constructor with record of type T and report messages
+   * @param processedRecord record that has been processed as result.
+   * @param reportMessages report of the processing of the record as result.
    */
   public ProcessedResult(T processedRecord, Set<ReportMessage> reportMessages) {
     this.processedRecord = processedRecord;
@@ -44,7 +44,7 @@ public class ProcessedResult<T> {
 
   /**
    * Constructor with a Processed Result
-   * @param processedResult
+   * @param processedResult contains a record, a report messages and record status encapsulated within.
    */
   public ProcessedResult(ProcessedResult<T> processedResult) {
     this.processedRecord = processedResult.processedRecord;
@@ -54,7 +54,7 @@ public class ProcessedResult<T> {
 
   /**
    * Obtain the record
-   * @return record
+   * @return record that has been set as a result of processing.
    */
   public T getProcessedRecord() {
     return processedRecord;
@@ -70,7 +70,8 @@ public class ProcessedResult<T> {
 
   /**
    * Obtain record status
-   * @return status of overall result
+   * @return status of overall the processed result if there is an error in the report messages
+   * returns STOP, otherwise it returns CONTINUE.
    */
   public RecordStatus getRecordStatus() {
     if (reportMessages.isEmpty()) {
