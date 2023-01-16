265c9427c52c45119198a7ada0293e0b77c7fa25
Simon Tzanakis <Simon.Tzanakis@europeana.eu>
2023-01-16 10:14:29 +0100

    MET-4866 Migrate to new open api and swagger

--- metis-dereference/metis-dereference-rest/src/main/java/eu/europeana/metis/dereference/rest/config/SwaggerConfig.java
+++ metis-dereference/metis-dereference-rest/src/main/java/eu/europeana/metis/dereference/rest/config/SwaggerConfig.java
@@ -1,92 +1,30 @@
 package eu.europeana.metis.dereference.rest.config;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import net.sf.saxon.s9api.streams.Predicates;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
-import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
-import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
-import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver;
-import org.springframework.boot.actuate.endpoint.web.EndpointMapping;
-import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
-import org.springframework.boot.actuate.endpoint.web.ExposableWebEndpoint;
-import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Config for Swagger documentation
  */
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
   /**
-   * The swagger api documentation docket.
+   * The open api documentation docket.
    *
    * @return the docket configuration
    */
   @Bean
-  public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .useDefaultResponseMessages(false)
-        .select()
-        .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework")))
-        .build()
-        .apiInfo(apiInfo());
-  }
-
-  private ApiInfo apiInfo() {
-    return new ApiInfo(
-        "Dereference REST API",
-        "Dereference REST API for Europeana",
-        "v1",
-        "API TOS",
-        new Contact("development", "europeana.eu", "development@europeana.eu"),
-        "EUPL Licence v1.2",
-        "https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12", Collections.emptyList());
-  }
-
-  //This is a hack because springfox-swagger and spring-boot-actuator do not work together.
-  //Keep in mind that the spring.mvc.pathmatch.matching-strategy=ant_path_matcher in the configuration might also be
-  //required. Furthermore, we should consider migrating to springdoc and openapi since springfox seems to be inactive.
-  @Bean
-  public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
-      ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier,
-      EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties,
-      Environment environment) {
-    List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
-    Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
-    allEndpoints.addAll(webEndpoints);
-    allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
-    allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
-    String basePath = webEndpointProperties.getBasePath();
-    EndpointMapping endpointMapping = new EndpointMapping(basePath);
-    boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
-    return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
-        corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping,
-        null);
-  }
-
-
-  private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment,
-      String basePath) {
-    return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.isNotBlank(basePath) || ManagementPortType.get(
-        environment).equals(
-        ManagementPortType.DIFFERENT));
+  public OpenAPI openAPI() {
+    return new OpenAPI().info(
+        new Info().title("Dereference REST API")
+                  .description("Dereference REST API for Europeana")
+                  .version("v1")
+                  .license(
+                      new License().name("EUPL Licence v1.2")
+                                   .url("https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12")));
   }
 }
