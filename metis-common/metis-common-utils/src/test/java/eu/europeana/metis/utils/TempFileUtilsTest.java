0f9e8b4f313e569612ab34319e8aca7f96c3a503
Simon Tzanakis <simon.tzanakis@europeana.eu>
2023-11-22 13:21:45 +0100

    MET-5473: Upgrade to jdk 21 builder (#639)
    MET-5473: Update to make build work again

--- metis-common/metis-common-utils/src/test/java/eu/europeana/metis/utils/TempFileUtilsTest.java
+++ metis-common/metis-common-utils/src/test/java/eu/europeana/metis/utils/TempFileUtilsTest.java
@@ -1,10 +1,9 @@
 package eu.europeana.metis.utils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,8 +11,9 @@ import java.nio.file.attribute.PosixFilePermission;
 import java.util.EnumSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TempFileUtilsTest {
 
@@ -27,22 +27,9 @@ class TempFileUtilsTest {
   }
 
   @Test
-  void createSecureTempFileDeleteOnExit()
-      throws ClassNotFoundException, IOException, NoSuchFieldException, IllegalAccessException {
+  void createSecureTempFileDeleteOnExit() throws IOException {
     final Path secureTempFile = TempFileUtils.createSecureTempFileDeleteOnExit("prefix", "suffix");
     assertFilePermissions(secureTempFile);
-
-    final LinkedHashSet<String> filesForDeletion = getFileListMarkedForDeletion();
-    assertTrue(filesForDeletion.contains(secureTempFile.toString()));
-  }
-
-  private LinkedHashSet<String> getFileListMarkedForDeletion()
-      throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
-    //Check that deletion on exit is in place
-    final Class<?> deleteOnExitHook = Class.forName("java.io.DeleteOnExitHook");
-    final Field filesField = deleteOnExitHook.getDeclaredField("files");
-    filesField.setAccessible(true);
-    return castList(filesField.get(null));
   }
 
   private LinkedHashSet<String> castList(Object objectList) {
