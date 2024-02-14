7b40c885240ebd9539ce1aa35ed91863be677ceb
Joana Sousa <joana.sousa@europeana.eu>
2024-02-14 14:55:25 +0100

    MET-5757 Updated 3d processor

--- metis-media-service/src/main/java/eu/europeana/metis/mediaprocessing/model/Media3dResourceMetadata.java
+++ metis-media-service/src/main/java/eu/europeana/metis/mediaprocessing/model/Media3dResourceMetadata.java
@@ -10,13 +10,7 @@ public class Media3dResourceMetadata extends AbstractResourceMetadata{
    * @param mimeType The resource mime type.
    * @param resourceUrl The resource URL.
    * @param contentSize The file content size.
-   * @param thumbnails The thumbnails generated for this text resource.
    */
-  public Media3dResourceMetadata(String mimeType, String resourceUrl, Long contentSize,
-      List<? extends Thumbnail> thumbnails) {
-    super(mimeType, resourceUrl, contentSize, thumbnails);
-  }
-
   public Media3dResourceMetadata(String mimeType, String resourceUrl, Long contentSize) {
     super(mimeType, resourceUrl, contentSize, null);
   }
