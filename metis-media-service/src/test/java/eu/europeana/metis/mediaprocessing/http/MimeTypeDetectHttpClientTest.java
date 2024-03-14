85f299819708098cdd1fa3fc90611291de9c0333
Joana Sousa <joana.sousa@europeana.eu>
2024-03-14 15:26:34 +0100

    MET-5763 Started writing unit tests

--- metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/http/MimeTypeDetectHttpClientTest.java
+++ metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/http/MimeTypeDetectHttpClientTest.java
@@ -3,12 +3,10 @@ package eu.europeana.metis.mediaprocessing.http;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.springframework.http.ContentDisposition;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
@@ -17,7 +15,7 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMoc
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MimeTypeDetectHttpClientTest {
-    private static final String EXPECTED_MIME_TYPE = "audio/mpeg";
+    private static final String EXPECTED_AUDIO_MIME_TYPE = "audio/mpeg";
     @RegisterExtension
     static WireMockExtension wireMockExtension = WireMockExtension.newInstance()
             .options(wireMockConfig().dynamicPort().dynamicHttpsPort())
@@ -51,7 +49,43 @@ class MimeTypeDetectHttpClientTest {
         String detectedMimeType = mimeTypeDetectHttpClient.download(new URL(url));
 
         // then
-        assertEquals(EXPECTED_MIME_TYPE, detectedMimeType);
+        assertEquals(EXPECTED_AUDIO_MIME_TYPE, detectedMimeType);
+    }
+
+    @Test
+    void download_detectMimeTypeStlAscii_expectSuccess() throws IOException {
+        // given
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("__files/block100.stl")) {
+            byte[] fileBytes = inputStream.readAllBytes();
+            wireMockExtension.stubFor(get("/imagen_id.do?idImagen=10610909").willReturn(aResponse()
+                .withStatus(200)
+                .withBody(fileBytes)
+                .withHeader("Content-Disposition", "inline; filename=\"block100.stl\"")));
+        }
+        final String url = String.format("http://localhost:%d/imagen_id.do?idImagen=10610909", wireMockExtension.getPort());
+        // when
+        String detectedMimeType = mimeTypeDetectHttpClient.download(new URL(url));
+
+        // then
+        assertEquals("model/x.stl-ascii", detectedMimeType);
+    }
+
+    @Test
+    void download_detectMimeTypeStlBinary_expectSuccess() throws IOException {
+        // given
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("__files/Cube_3d_printing_sample.stl")) {
+            byte[] fileBytes = inputStream.readAllBytes();
+            wireMockExtension.stubFor(get("/imagen_id.do?idImagen=10610909").willReturn(aResponse()
+                .withStatus(200)
+                .withBody(fileBytes)
+                .withHeader("Content-Disposition", "inline; filename=\"Cube_3d_printing_sample.stl\"")));
+        }
+        final String url = String.format("http://localhost:%d/imagen_id.do?idImagen=10610909", wireMockExtension.getPort());
+        // when
+        String detectedMimeType = mimeTypeDetectHttpClient.download(new URL(url));
+
+        // then
+        assertEquals("model/x.stl-binary", detectedMimeType);
     }
 
 }
