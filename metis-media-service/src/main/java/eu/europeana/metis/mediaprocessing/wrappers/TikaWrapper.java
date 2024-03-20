63f007815fb9f3ff8efc3d5c9514168db38f63cd
Joana Sousa <joana.sousa@europeana.eu>
2024-03-20 16:17:51 +0100

    MET-5763 Added javadoc

--- metis-media-service/src/main/java/eu/europeana/metis/mediaprocessing/wrappers/TikaWrapper.java
+++ metis-media-service/src/main/java/eu/europeana/metis/mediaprocessing/wrappers/TikaWrapper.java
@@ -5,14 +5,27 @@ import java.io.InputStream;
 import org.apache.tika.Tika;
 import org.apache.tika.metadata.Metadata;
 
+/**
+ * Wrapper class of Tika
+ */
 public class TikaWrapper {
 
   private final Tika tika;
 
+  /**
+   * It creates a new instance of Tika
+   */
   public TikaWrapper() {
     this.tika = new Tika();
   }
 
+  /**
+   * It uses tika's own detect method
+   * @param inputStream The input stream to detect from
+   * @param metadata The metadata associated with the input stream
+   * @return The mime type detected from the input stream
+   * @throws IOException
+   */
   public String detect(InputStream inputStream, Metadata metadata) throws IOException {
 
     String detectedMimeType = tika.detect(inputStream, metadata);
