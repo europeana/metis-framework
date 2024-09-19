a316a9ad2ed5f0bd7b8f981c048da1745ef57c05
Jorge Ortiz <jorge.ortiz@europeana.eu>
2024-09-19 15:58:15 +0200

    MET-5960 update unused exceptions

--- metis-debias/metis-debias-detect-rest/src/main/java/eu/europeana/metis/debias/detect/rest/exceptions/ExceptionResponseHandler.java
+++ metis-debias/metis-debias-detect-rest/src/main/java/eu/europeana/metis/debias/detect/rest/exceptions/ExceptionResponseHandler.java
@@ -2,8 +2,11 @@ package eu.europeana.metis.debias.detect.rest.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.net.URISyntaxException;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -52,6 +55,38 @@ public class ExceptionResponseHandler {
     HttpStatus status = annotationResponseStatus == null ? HttpStatus.INTERNAL_SERVER_ERROR
         : annotationResponseStatus.value();
     response.setStatus(status.value());
+    return new ServerError(response.getStatus(), exception.getMessage());
+  }
+
+  /**
+   * Handle media not supported response server error.
+   *
+   * @param response the response
+   * @param request the request
+   * @param exception the exception
+   * @return the server error
+   */
+  @ResponseBody
+  @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+  public ServerError handleResponse(HttpServletResponse response, HttpServletRequest request,
+      HttpMediaTypeNotSupportedException exception) {
+    response.setStatus(HttpStatus.BAD_REQUEST.value());
+    return new ServerError(response.getStatus(), exception.getMessage());
+  }
+
+  /**
+   * Handle response server error.
+   *
+   * @param response the response
+   * @param request the request
+   * @param exception the exception
+   * @return the server error
+   */
+  @ResponseBody
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ServerError handleResponse(HttpServletResponse response, HttpServletRequest request,
+      HttpMessageNotReadableException exception) {
+    response.setStatus(HttpStatus.BAD_REQUEST.value());
     return new ServerError(response.getStatus(), exception.getMessage());
   }
 }
