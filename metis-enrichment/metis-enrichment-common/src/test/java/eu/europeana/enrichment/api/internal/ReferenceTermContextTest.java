17f8e2a046693299df66726e44a427f1f5260eb9
Jorge Ortiz <jorge.ortiz@europeana.eu>
2025-04-29 14:43:31 +0200

    MET-6423 update unit test

--- metis-enrichment/metis-enrichment-common/src/test/java/eu/europeana/enrichment/api/internal/ReferenceTermContextTest.java
+++ metis-enrichment/metis-enrichment-common/src/test/java/eu/europeana/enrichment/api/internal/ReferenceTermContextTest.java
@@ -1,6 +1,6 @@
 package eu.europeana.enrichment.api.internal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Set;
 import org.junit.jupiter.api.Test;
@@ -9,7 +9,8 @@ class ReferenceTermContextTest {
 
   @Test
   void createFromString() {
-    ReferenceTermContext referenceTermContext = ReferenceTermContext.createFromString("http://reference", Set.of(ProxyFieldType.DCTERMS_SPATIAL));
+    ReferenceTermContext referenceTermContext = ReferenceTermContext.createFromString("http://reference",
+        Set.of(ProxyFieldType.DCTERMS_SPATIAL));
     assertEquals("http://reference", referenceTermContext.getReferenceAsString());
     assertEquals(1, referenceTermContext.getFieldTypes().size());
     assertEquals(ProxyFieldType.DCTERMS_SPATIAL, referenceTermContext.getFieldTypes().iterator().next());
@@ -17,14 +18,17 @@ class ReferenceTermContextTest {
 
   @Test
   void referenceEquals() {
-    ReferenceTermContext referenceTermContext = ReferenceTermContext.createFromString("http://reference", Set.of(ProxyFieldType.DCTERMS_SPATIAL));
-    ReferenceTermContext referenceTermContext2 = ReferenceTermContext.createFromString("http://reference", Set.of(ProxyFieldType.DCTERMS_SPATIAL));
+    ReferenceTermContext referenceTermContext = ReferenceTermContext.createFromString("http://reference",
+        Set.of(ProxyFieldType.DCTERMS_SPATIAL));
+    ReferenceTermContext referenceTermContext2 = ReferenceTermContext.createFromString("http://reference",
+        Set.of(ProxyFieldType.DCTERMS_SPATIAL));
     assertEquals(referenceTermContext, referenceTermContext2);
   }
 
   @Test
   void getCandidateTypes() {
-    ReferenceTermContext referenceTermContext = ReferenceTermContext.createFromString("http://reference", Set.of(ProxyFieldType.DCTERMS_SPATIAL));
+    ReferenceTermContext referenceTermContext = ReferenceTermContext.createFromString("http://reference",
+        Set.of(ProxyFieldType.DCTERMS_SPATIAL));
     assertEquals(1, referenceTermContext.getCandidateTypes().size());
     assertEquals(ProxyFieldType.DCTERMS_SPATIAL.getEntityType(), referenceTermContext.getCandidateTypes().iterator().next());
     assertEquals("http://reference", referenceTermContext.getReferenceAsString());
