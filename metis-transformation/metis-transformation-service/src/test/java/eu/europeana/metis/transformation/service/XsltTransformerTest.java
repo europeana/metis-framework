224276881e9286557449776e5ad30fd6b84f70fe
Simon Tzanakis <simon.tzanakis@europeana.eu>
2025-08-08 10:49:00 +0200

    feat/MET-6597_copy_pid_value_in_reorder_and_transform (#735)
    * MET-6597 Copy pid fields to reorder xslt
    
    * MET-6597 Update default xslt for pids
    
    * MET-6597 Process review

--- metis-transformation/metis-transformation-service/src/test/java/eu/europeana/metis/transformation/service/XsltTransformerTest.java
+++ metis-transformation/metis-transformation-service/src/test/java/eu/europeana/metis/transformation/service/XsltTransformerTest.java
@@ -1,77 +1,119 @@
 package eu.europeana.metis.transformation.service;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Objects;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+import org.xmlunit.builder.DiffBuilder;
+import org.xmlunit.diff.Diff;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class XsltTransformerTest {
 
-  @Test
-  void shouldTransformGivenFile()
-      throws TransformationException, IOException, ParserConfigurationException, SAXException {
-    final URL xsltFile = getClass().getClassLoader().getResource("sample_xslt.xslt");
-    assertNotNull(xsltFile);
-    assertDoesNotThrow(xsltFile::toURI);
-    final byte[] fileToTransform = readFile("xmlForTesting.xml");
-    final StringWriter stringWriter = new XsltTransformer(xsltFile.toString(), xsltFile.openStream()).transform(
-        fileToTransform, null);
-    final String transformed = stringWriter.toString();
+    @Test
+    void shouldTransformGivenFile() throws Exception {
+        final InputStream xsltFileInputStream = readFileToInputStream("sample_xslt.xslt");
+        final String fileToTransform = readFileToString("xmlForTesting.xml");
+        final StringWriter stringWriter = new XsltTransformer("sample_xslt.xslt", xsltFileInputStream).transform(
+                fileToTransform.getBytes(), null);
+        final String transformed = stringWriter.toString();
 
-    final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-    final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-    final Document doc = dBuilder.parse(new InputSource(new StringReader(transformed)));
-    assertEquals("html", doc.getDocumentElement().getNodeName());
-    assertEquals(1, doc.getElementsByTagName("h2").getLength());
-    assertEquals(1, doc.getElementsByTagName("table").getLength());
-    assertEquals(2, doc.getElementsByTagName("th").getLength());
-    assertEquals(26 + 1, doc.getElementsByTagName("tr").getLength());
-    assertEquals(2 * 26, doc.getElementsByTagName("td").getLength());
-  }
+        final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        final Document doc = dBuilder.parse(new InputSource(new StringReader(transformed)));
+        assertEquals("html", doc.getDocumentElement().getNodeName());
+        assertEquals(1, doc.getElementsByTagName("h2").getLength());
+        assertEquals(1, doc.getElementsByTagName("table").getLength());
+        assertEquals(2, doc.getElementsByTagName("th").getLength());
+        assertEquals(26 + 1, doc.getElementsByTagName("tr").getLength());
+        assertEquals(2 * 26, doc.getElementsByTagName("td").getLength());
+    }
 
-  @Test
-  void shouldTransformGivenFileWithInjection() throws IOException, TransformationException {
-    URL xsltFile = getClass().getClassLoader().getResource("inject_node.xslt");
-    byte[] fileToTransform = readFile("xmlForTestingParamInjection.xml");
-    StringWriter wr = new XsltTransformer(xsltFile.toString(), xsltFile.openStream(), "sample",
-        null, null)
-        .transform(fileToTransform, null);
-    String resultXml = wr.toString();
-    assertTrue(resultXml.contains("<injected_node>"));
-    assertTrue(resultXml.contains("sample"));
-    assertTrue(resultXml.contains("</injected_node>"));
-  }
+    @Test
+    void shouldTransformGivenFileWithInjection() throws Exception {
+        final InputStream xsltFileInputStream = readFileToInputStream("inject_node.xslt");
+        final String fileToTransform = readFileToString("xmlForTestingParamInjection.xml");
+        StringWriter stringWriter = new XsltTransformer("inject_node.xslt", xsltFileInputStream, "sample",
+                null, null)
+                .transform(fileToTransform.getBytes(), null);
+        final String resultXml = stringWriter.toString();
+        assertTrue(resultXml.contains("<injected_node>"));
+        assertTrue(resultXml.contains("sample"));
+        assertTrue(resultXml.contains("</injected_node>"));
+    }
 
-  @Test
-  void shouldFailForMalformedFile() throws Exception {
-    URL xsltFile = getClass().getClassLoader().getResource("inject_node.xslt");
-    byte[] fileToTransform = readFile("malformedFile.xml");
-    final XsltTransformer xsltTransformer = new XsltTransformer(xsltFile.toString(), xsltFile.openStream(), "sample", null, null);
-    assertThrows(TransformationException.class, () -> xsltTransformer.transform(fileToTransform, null));
-  }
+    @Test
+    void shouldFailForMalformedFile() throws Exception {
+        final InputStream xsltFileInputStream = readFileToInputStream("inject_node.xslt");
+        final String fileToTransform = readFileToString("malformedFile.xml");
+        final XsltTransformer xsltTransformer = new XsltTransformer("inject_node.xslt", xsltFileInputStream, "sample", null, null);
+        assertThrows(TransformationException.class, () -> xsltTransformer
+                .transform(fileToTransform.getBytes(), null));
+    }
 
-  private byte[] readFile(String fileName) throws IOException {
-    final String xml = IOUtils.toString(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream(fileName)),
-        StandardCharsets.UTF_8);
-    final byte[] bytes = xml.getBytes(StandardCharsets.UTF_8);
-    return IOUtils.toByteArray(new ByteArrayInputStream(bytes));
-  }
+    @Test
+    void reorderEDM() throws Exception {
+        final InputStream xsltFileInputStream = readFileToInputStream("edm_sorter.xsl");
+        final String fileToTransform = readFileToString("europeana_record_edm_external_unsorted.xml");
+        final StringWriter stringWriter = new XsltTransformer("edm_sorter.xsl", xsltFileInputStream)
+                .transform(fileToTransform.getBytes(), null);
+        final String transformed = stringWriter.toString();
+        final String expected = readFileToString("europeana_record_edm_external_sorted.xml");
+
+        Diff diff = DiffBuilder.compare(expected)
+                .withTest(transformed)
+                .ignoreWhitespace()
+                .ignoreComments()
+                .checkForIdentical()
+                .build();
+
+        assertFalse(diff.hasDifferences(), () -> "XMLs differ: " + diff);
+    }
+
+    @Test
+    void transformToEDMInternal() throws Exception {
+        final InputStream xsltFileInputStream = readFileToInputStream("default_transformation.xslt");
+        final String fileToTransform = readFileToString("europeana_record_edm_external_sorted.xml");
+        final StringWriter stringWriter = new XsltTransformer("default_transformation.xslt", xsltFileInputStream)
+                .transform(fileToTransform.getBytes(), null);
+        final String transformed = stringWriter.toString();
+        final String expected = readFileToString("europeana_record_edm_internal.xml");
+
+        Diff diff = DiffBuilder.compare(expected)
+                .withTest(transformed)
+                .ignoreWhitespace()
+                .ignoreComments()
+                .checkForIdentical()
+                .build();
+
+        assertFalse(diff.hasDifferences(), () -> "XMLs differ: " + diff);
+    }
+
+    public String readFileToString(String file) throws IOException {
+        InputStream inputStream = readFileToInputStream(file);
+        return new BufferedReader(new InputStreamReader(inputStream))
+                .lines().collect(Collectors.joining("\n"));
+    }
+
+    private InputStream readFileToInputStream(String file) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(file);
+        if (inputStream == null) {
+            throw new IOException("Failed reading file " + file);
+        }
+        return inputStream;
+    }
 }
