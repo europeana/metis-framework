901207c4e8c8aac18b51858718ded9e31b70cad7
Simon Tzanakis <simon.tzanakis@europeana.eu>
2025-11-12 13:09:42 +0100

    MET-6887 Refactor, add support for LAS files

--- metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/http/MimeTypeDetectHttpClientTest.java
+++ metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/http/MimeTypeDetectHttpClientTest.java
@@ -4,181 +4,96 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 class MimeTypeDetectHttpClientTest {
-    private static final String EXPECTED_AUDIO_MIME_TYPE = "audio/mpeg";
-    @RegisterExtension
-    static WireMockExtension wireMockExtension = WireMockExtension.newInstance()
-            .options(wireMockConfig().dynamicPort().dynamicHttpsPort())
-            .build();
+
+  private static final String EXPECTED_AUDIO_MIME_TYPE = "audio/mpeg";
+
+  @RegisterExtension
+  static WireMockExtension wireMockExtension = WireMockExtension.newInstance()
+                                                                .options(wireMockConfig().dynamicPort().dynamicHttpsPort())
+                                                                .build();
 
   private final MimeTypeDetectHttpClient mimeTypeDetectHttpClient
       = new MimeTypeDetectHttpClient(5000, 5000, 5000);
 
   @Test
   void getResourceUrl() throws MalformedURLException, URISyntaxException {
-    // when
     String resultUrl = mimeTypeDetectHttpClient.getResourceUrl(new URI("http://localhost").toURL());
-
-    // then
     assertEquals("http://localhost", resultUrl);
   }
 
-  @Test
-  void download_withContentDisposition_expectSuccess() throws IOException, URISyntaxException {
-    // given
-    try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("__files/audio_test.mp3")) {
-      byte[] audioBytes = inputStream.readAllBytes();
-      wireMockExtension.stubFor(get("/imagen_id.do?idImagen=10610909").willReturn(aResponse()
+  @ParameterizedTest(name = "{index} => file={0}, expected={1}")
+  @MethodSource
+  void download_detectMimeType(String resourcePath, String expectedMimeType, Map<String, String> extraHeaders)
+      throws IOException, URISyntaxException {
+
+    // Read file bytes
+    try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
+      assertNotNull(inputStream, "Test resource not found: " + resourcePath);
+      byte[] fileBytes = inputStream.readAllBytes();
+
+      // Build the base response
+      ResponseDefinitionBuilder responseDefinitionBuilder = aResponse()
           .withStatus(200)
-          .withBody(audioBytes)
-          .withHeader("Content-Disposition", "inline; filename=\"F34-0003_0001.mp3\"")
-          .withHeader("Content-Type", "audio/mpeg")));
+          .withBody(fileBytes)
+          .withHeader("Content-Disposition", "inline; filename=\"" + extractFileName(resourcePath) + "\"");
+
+      // Add optional headers if provided
+      if (extraHeaders != null) {
+        extraHeaders.forEach(responseDefinitionBuilder::withHeader);
+      }
+
+      wireMockExtension.stubFor(get("/path_to_file").willReturn(responseDefinitionBuilder));
     }
-    final String url = String.format("http://localhost:%d/imagen_id.do?idImagen=10610909", wireMockExtension.getPort());
-    // when
+
+    String url = String.format("http://localhost:%d/path_to_file",
+        wireMockExtension.getPort());
     String detectedMimeType = mimeTypeDetectHttpClient.download(new URI(url).toURL());
 
-        // then
-        assertEquals(EXPECTED_AUDIO_MIME_TYPE, detectedMimeType);
-    }
-
-    @Test
-    void download_detectMimeTypeStlAscii_expectSuccess() throws IOException, URISyntaxException {
-        // given
-        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("__files/block100.stl")) {
-            byte[] fileBytes = inputStream.readAllBytes();
-            wireMockExtension.stubFor(get("/imagen_id.do?idImagen=10610909").willReturn(aResponse()
-                .withStatus(200)
-                .withBody(fileBytes)
-                .withHeader("Content-Disposition", "inline; filename=\"block100.stl\"")));
-        }
-        final String url = String.format("http://localhost:%d/imagen_id.do?idImagen=10610909", wireMockExtension.getPort());
-        // when
-        String detectedMimeType = mimeTypeDetectHttpClient.download(new URI(url).toURL());
-
-        // then
-        assertEquals("model/x.stl-ascii", detectedMimeType);
-    }
-
-    @Test
-    void download_detectMimeTypeStlBinary_expectSuccess() throws IOException, URISyntaxException {
-        // given
-        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("__files/Cube_3d_printing_sample.stl")) {
-            byte[] fileBytes = inputStream.readAllBytes();
-            wireMockExtension.stubFor(get("/imagen_id.do?idImagen=10610909").willReturn(aResponse()
-                .withStatus(200)
-                .withBody(fileBytes)
-                .withHeader("Content-Disposition", "inline; filename=\"Cube_3d_printing_sample.stl\"")));
-        }
-        final String url = String.format("http://localhost:%d/imagen_id.do?idImagen=10610909", wireMockExtension.getPort());
-        // when
-        String detectedMimeType = mimeTypeDetectHttpClient.download(new URI(url).toURL());
-
-        // then
-        assertEquals("model/x.stl-binary", detectedMimeType);
-    }
-
-    @Test
-    void download_returnProvidedStlMimeType_expectSuccess() throws IOException, URISyntaxException {
-        // given
-        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("__files/Cube_3d_printing_sample.stl")) {
-            byte[] fileBytes = inputStream.readAllBytes();
-            wireMockExtension.stubFor(get("/imagen_id.do?idImagen=10610909").willReturn(aResponse()
-                .withStatus(200)
-                .withBody(fileBytes)
-                .withHeader("Content-Disposition", "inline; filename=\"Cube_3d_printing_sample.stl\"")
-                .withHeader("Content-Type", "model/stl")));
-        }
-        final String url = String.format("http://localhost:%d/imagen_id.do?idImagen=10610909", wireMockExtension.getPort());
-        // when
-        String detectedMimeType = mimeTypeDetectHttpClient.download(new URI(url).toURL());
-
-        // then
-        assertEquals("model/x.stl-binary", detectedMimeType);
-    }
-
-  @Test
-  void download_detectMimeTypeGltf_expectSuccess() throws IOException, URISyntaxException {
-    // given
-    try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("__files/adamHead.gltf")) {
-      byte[] audioBytes = inputStream.readAllBytes();
-      wireMockExtension.stubFor(get("/imagen_id.do?idImagen=10610909").willReturn(aResponse()
-          .withStatus(200)
-          .withBody(audioBytes)
-          .withHeader("Content-Disposition", "inline; filename=\"adamHead.gltf\"")));
-    }
-    final String url = String.format("http://localhost:%d/imagen_id.do?idImagen=10610909", wireMockExtension.getPort());
-    // when
-    String detectedMimeType = mimeTypeDetectHttpClient.download(new URI(url).toURL());
-
-    // then
-    assertEquals("model/gltf+json", detectedMimeType);
+    assertEquals(expectedMimeType, detectedMimeType);
   }
 
-  @Test
-  void download_detectMimeTypeGlb_expectSuccess() throws IOException, URISyntaxException {
-    // given
-    try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("__files/Duck.glb")) {
-      byte[] audioBytes = inputStream.readAllBytes();
-      wireMockExtension.stubFor(get("/imagen_id.do?idImagen=10610909").willReturn(aResponse()
-          .withStatus(200)
-          .withBody(audioBytes)
-          .withHeader("Content-Disposition", "inline; filename=\"Duck.glb\"")));
-    }
-    final String url = String.format("http://localhost:%d/imagen_id.do?idImagen=10610909", wireMockExtension.getPort());
-    // when
-    String detectedMimeType = mimeTypeDetectHttpClient.download(new URI(url).toURL());
-
-    // then
-    assertEquals("model/gltf-binary", detectedMimeType);
+  private static String extractFileName(String path) {
+    return path.substring(path.lastIndexOf('/') + 1);
   }
 
-  @Test
-  void download_detectMimeTypeOembedJson_expectSuccess() throws IOException, URISyntaxException {
-    // given
-    try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("__files/oembed.json")) {
-      byte[] jsonBytes = inputStream.readAllBytes();
-      wireMockExtension.stubFor(get("/api/oembed.json?url=https://vimeo.com/24416915")
-          .willReturn(aResponse()
-          .withStatus(200)
-          .withBody(jsonBytes)
-          .withHeader("Content-Disposition", "inline; filename=\"oembed.json\"")));
-    }
-    final String url = String.format("http://localhost:%d/api/oembed.json?url=https://vimeo.com/24416915", wireMockExtension.getPort());
-    // when
-    String detectedMimeType = mimeTypeDetectHttpClient.download(new URI(url).toURL());
-
-    // then
-    assertEquals("application/json", detectedMimeType);
-  }
-
-  @Test
-  void download_detectMimeTypeOembedXml_expectSuccess() throws IOException, URISyntaxException {
-    // given
-    try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("__files/oembed.xml")) {
-      byte[] xmlBytes = inputStream.readAllBytes();
-      wireMockExtension.stubFor(get("/api/oembed.xml?url=https://vimeo.com/24416915")
-          .willReturn(aResponse()
-          .withStatus(200)
-          .withBody(xmlBytes)
-          .withHeader("Content-Disposition", "inline; filename=\"oembed.xml\"")));
-    }
-    final String url = String.format("http://localhost:%d/api/oembed.xml?url=https://vimeo.com/24416915", wireMockExtension.getPort());
-    // when
-    String detectedMimeType = mimeTypeDetectHttpClient.download(new URI(url).toURL());
-
-    // then
-    assertEquals("application/xml", detectedMimeType);
+  private static Stream<Arguments> download_detectMimeType() {
+    return Stream.of(
+        //MPEG
+        of("__files/audio_test.mp3", EXPECTED_AUDIO_MIME_TYPE, Map.of("Content-Type", "audio/mpeg")),
+        //STL
+        of("__files/3d/block100.stl", "model/x.stl-ascii", null),
+        of("__files/3d/cube.stl", "model/x.stl-binary", null),
+        of("__files/3d/cube.stl", "model/x.stl-binary", Map.of("Content-Type", "model/stl")),
+        //LAS
+        of("__files/3d/cube_8_points.las", "application/vnd.las", null),
+        //LAZ
+        of("__files/3d/cube_8_points.laz", "application/vnd.laszip", null),
+        //GLTF
+        of("__files/3d/adamHead.gltf", "model/gltf+json", null),
+        of("__files/3d/Duck.glb", "model/gltf-binary", null),
+        //OEMBED
+        of("__files/oembed.json", "application/json", null),
+        of("__files/oembed.xml", "application/xml", null)
+    );
   }
 }
---
+++ metis-media-service/src/test/resources/__files/3d/readme.txt
@@ -0,0 +1,5 @@
+Online tools for rendering 3d models.
+LAS/LAZ: https://plas.io/
+
+Tools used to generate the models:
+LAS/LAZ -> LAStools: https://github.com/LAStools/LAStools
