26bcfc6dee8571b6010a8ae9e84a2482b1f8c5ef
Simon Tzanakis <simon.tzanakis@europeana.eu>
2025-11-14 12:13:40 +0100

    MET-6887 Cleanup

--- metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/http/MimeTypeDetectHttpClientTest.java
+++ metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/http/MimeTypeDetectHttpClientTest.java
@@ -7,14 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.params.provider.Arguments.of;
 
-import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -41,7 +39,7 @@ class MimeTypeDetectHttpClientTest {
 
   @ParameterizedTest(name = "{index} => file={0}, expected={1}")
   @MethodSource
-  void download_detectMimeType(String resourcePath, String expectedMimeType, Map<String, String> extraHeaders)
+  void download_detectMimeType(String resourcePath, String expectedMimeType)
       throws IOException, URISyntaxException {
 
     // Read file bytes
@@ -49,18 +47,13 @@ class MimeTypeDetectHttpClientTest {
       assertNotNull(inputStream, "Test resource not found: " + resourcePath);
       byte[] fileBytes = inputStream.readAllBytes();
 
-      // Build the base response
-      ResponseDefinitionBuilder responseDefinitionBuilder = aResponse()
-          .withStatus(200)
-          .withBody(fileBytes)
-          .withHeader("Content-Disposition", "inline; filename=\"" + extractFileName(resourcePath) + "\"");
-
-      // Add optional headers if provided
-      if (extraHeaders != null) {
-        extraHeaders.forEach(responseDefinitionBuilder::withHeader);
-      }
-
-      wireMockExtension.stubFor(get("/path_to_file").willReturn(responseDefinitionBuilder));
+      wireMockExtension.stubFor(get("/path_to_file").willReturn(
+          aResponse()
+              .withStatus(200)
+              .withBody(fileBytes)
+              .withHeader("Content-Disposition",
+                  "inline; filename=\"" + extractFileName(resourcePath) + "\"")
+      ));
     }
 
     String url = String.format("http://localhost:%d/path_to_file",
@@ -77,42 +70,42 @@ class MimeTypeDetectHttpClientTest {
   private static Stream<Arguments> download_detectMimeType() {
     return Stream.of(
         //MPEG
-        of("__files/audio_test.mp3", "audio/mpeg", Map.of("Content-Type", "audio/mpeg")),
+        of("__files/audio_test.mp3", "audio/mpeg"),
         //LAS
-        of("__files/3d/cube_8_points.las", "application/vnd.las", null),
+        of("__files/3d/cube_8_points.las", "application/vnd.las"),
         //LAZ
-        of("__files/3d/cube_8_points.laz", "application/vnd.laszip", null),
+        of("__files/3d/cube_8_points.laz", "application/vnd.laszip"),
         //E57
-        of("__files/3d/cube_8_points.e57", "model/e57", null),
+        of("__files/3d/cube_8_points.e57", "model/e57"),
         //DXF
-        of("__files/3d/cube.dxf", "image/vnd.dxf", null),
+        of("__files/3d/cube.dxf", "image/vnd.dxf"),
         //OBJ
-        of("__files/3d/cube.obj", "model/obj", null),
+        of("__files/3d/cube.obj", "model/obj"),
         //DAE
-        of("__files/3d/cube.dae", "model/vnd.collada+xml", null),
+        of("__files/3d/cube.dae", "model/vnd.collada+xml"),
         //STL
-        of("__files/3d/cube-ascii.stl", "model/x.stl-ascii", null),
-        of("__files/3d/cube-binary.stl", "model/x.stl-binary", null),
-        of("__files/3d/cube-binary.stl", "model/x.stl-binary", Map.of("Content-Type", "model/stl")),
+        of("__files/3d/cube-ascii.stl", "model/x.stl-ascii"),
+        of("__files/3d/cube-binary.stl", "model/x.stl-binary"),
+        of("__files/3d/cube-binary.stl", "model/x.stl-binary"),
         //IGES
-        of("__files/3d/cube.iges", "model/iges", null),
+        of("__files/3d/cube.iges", "model/iges"),
         //STEP
-        of("__files/3d/cube.stp", "model/step", null),
-        of("__files/3d/cube.stpz", "model/step+zip", null),
+        of("__files/3d/cube.stp", "model/step"),
+        of("__files/3d/cube.stpz", "model/step+zip"),
         //VRML
-        of("__files/3d/cube.vrml", "model/vrml", null),
+        of("__files/3d/cube.vrml", "model/vrml"),
         //X3D
-        of("__files/3d/cube.x3d", "model/x3d+xml", null),
-        of("__files/3d/cube.x3dv", "model/x3d-vrml", null),
+        of("__files/3d/cube.x3d", "model/x3d+xml"),
+        of("__files/3d/cube.x3dv", "model/x3d-vrml"),
         //GLTF
-        of("__files/3d/cube.gltf", "model/gltf+json", null),
-        of("__files/3d/cube.glb", "model/gltf-binary", null),
+        of("__files/3d/cube.gltf", "model/gltf+json"),
+        of("__files/3d/cube.glb", "model/gltf-binary"),
         //OEMBED
-        of("__files/oembed.json", "application/json", null),
-        of("__files/oembed.xml", "application/xml", null),
+        of("__files/oembed.json", "application/json"),
+        of("__files/oembed.xml", "application/xml"),
         //USD
-        of("__files/3d/cube.usda", "model/vnd.usda", null),
-        of("__files/3d/cube.usdz", "model/vnd.usdz+zip", null)
+        of("__files/3d/cube.usda", "model/vnd.usda"),
+        of("__files/3d/cube.usdz", "model/vnd.usdz+zip")
     );
   }
 }
