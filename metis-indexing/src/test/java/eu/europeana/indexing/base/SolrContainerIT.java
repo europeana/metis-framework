d764011190b73e43a092429daed8a325225ec25f
Jorge Ortiz <jorge.ortiz@europeana.eu>
2025-12-12 12:37:24 +0100

    MET-6438 update solr with schema direct from github and use of aliasing (#765)

--- metis-indexing/src/test/java/eu/europeana/indexing/base/SolrContainerIT.java
+++ metis-indexing/src/test/java/eu/europeana/indexing/base/SolrContainerIT.java
@@ -1,10 +1,17 @@
 package eu.europeana.indexing.base;
 
+import eu.europeana.metis.utils.TempFileUtils;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.List;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.UrlResource;
 import org.springframework.test.context.DynamicPropertyRegistry;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 
 /**
  * The type Solr container it.
@@ -23,20 +30,51 @@ public class SolrContainerIT extends TestContainer {
    */
   public static final String SOLR_COLLECTION_NAME = "solr_publish_test";
 
-
+  private static final String GITHUB_RAW_BASE_URL = "https://raw.githubusercontent.com/europeana/search/refs/heads/master/solr_confs/metadata/conf/";
+  private static final String QUERY_ALIASES_FILE = "query_aliases.xml";
+  private static final String GITHUB_RAW_QUERY_ALIASES_CONFIG_URL = GITHUB_RAW_BASE_URL + QUERY_ALIASES_FILE;
+  private static final String GITHUB_RAW_ENUMS_CONFIG_URL = GITHUB_RAW_BASE_URL + "enumsConfig.xml";
+  private static final String GITHUB_RAW_ELEVATE_CONFIG_URL = GITHUB_RAW_BASE_URL + "elevate.xml";
+  private static final String GITHUB_RAW_SOLR_SCHEMA_URL = GITHUB_RAW_BASE_URL + "schema.xml";
+  private static final String GITHUB_RAW_SOLR_CONFIG_URL = GITHUB_RAW_BASE_URL + "solrconfig.xml";
+  private static final String ALIASING_LIB_JAR = "europeana-collection-aliasing-solr6.6.5-0.0.1-SNAPSHOT.jar";
+  private static final String ALIASING_LIB_CONTAINER_PATH = "/opt/solr/contrib/lib/" + ALIASING_LIB_JAR;
+  private static final String ALIASING_LIB_RESOURCE_PATH = "solr/"+ ALIASING_LIB_JAR;
+  private static final String QUERY_ALIASES_PATH = "/opt/solr/server/solr/solr_publish_test_shard1_replica_n1/conf/" + QUERY_ALIASES_FILE;
+  private static final String FILE_PERMISSIONS = "644";
+  private static final String OWNERSHIP_COMMAND = "chown -R solr:solr /opt/solr/server/solr && chown -R solr:solr /opt/solr/contrib/lib";
   /**
    * Instantiates a new Solr container it.
    */
   public SolrContainerIT() {
-    solrContainer = new SolrMetisContainer(DockerImageName.parse(SOLR_VERSION))
-        .withCollection(SOLR_COLLECTION_NAME)
-        .withConfiguration("test",
-            getClass().getClassLoader().getResource("solr/solrconfig.xml"))
-        //At the moment this container doesn't support solr.AliasingSearchHandler is a custom plugin
-        .withSchema(getClass().getClassLoader().getResource("solr/schema.xml"))
-        .withConfigSchemaFiles(List.of(getClass().getClassLoader().getResource("solr/enumsConfig.xml"),
-            getClass().getClassLoader().getResource("solr/query_aliases.xml"),
-            getClass().getClassLoader().getResource("solr/elevate.xml")));
+    try {
+      File temp = TempFileUtils.createSecureTempFile("query_aliases", ".xml").toFile();
+      FileUtils.copyURLToFile(new UrlResource(URI.create(GITHUB_RAW_QUERY_ALIASES_CONFIG_URL)).getURL(), temp);
+      ImageFromDockerfile image = new ImageFromDockerfile()
+          .withDockerfileFromBuilder(builder ->
+              builder.from(SOLR_VERSION)
+                     .user("root")
+                     .copy(ALIASING_LIB_JAR, ALIASING_LIB_CONTAINER_PATH)
+                     .copy(QUERY_ALIASES_FILE, QUERY_ALIASES_PATH)
+                     .run(OWNERSHIP_COMMAND)
+                     .user("solr")
+                     .build())
+          .withFileFromFile(QUERY_ALIASES_FILE, temp, Integer.parseInt(FILE_PERMISSIONS, 8))
+          .withFileFromClasspath(ALIASING_LIB_JAR, ALIASING_LIB_RESOURCE_PATH);
+
+      solrContainer = new SolrMetisContainer(image)
+          .withCollection(SOLR_COLLECTION_NAME)
+          .withConfiguration("test", new UrlResource(URI.create(GITHUB_RAW_SOLR_CONFIG_URL)).getURL())
+          .withSchema(new UrlResource(URI.create(GITHUB_RAW_SOLR_SCHEMA_URL)).getURL())
+          .withConfigSchemaFiles(List.of(new UrlResource(URI.create(GITHUB_RAW_ENUMS_CONFIG_URL)).getURL(),
+              new UrlResource(URI.create(GITHUB_RAW_QUERY_ALIASES_CONFIG_URL)).getURL(),
+              new UrlResource(URI.create(GITHUB_RAW_ELEVATE_CONFIG_URL)).getURL()));
+
+    } catch (MalformedURLException e) {
+      throw new RuntimeException("Failed to create SolrMetisContainer due to malformed URL", e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
 
     solrContainer.start();
 
