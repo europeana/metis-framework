09bccb04a605cc87b946e8621394e9ccc49d5e18
Jorge Ortiz <jorge.ortiz@europeana.eu>
2026-01-27 13:13:07 +0100

    MET-6921 merge integration test and work code review comments

--- metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/extraction/MediaExtractorImplITTest.java
+++ metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/extraction/MediaExtractorImplITTest.java
@@ -1,5 +1,7 @@
 package eu.europeana.metis.mediaprocessing.extraction;
 
+import static eu.europeana.metis.mediaprocessing.model.RdfResourceKind.IIIF;
+import static eu.europeana.metis.mediaprocessing.model.RdfResourceKind.STANDARD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -8,14 +10,15 @@ import eu.europeana.metis.mediaprocessing.MediaProcessorFactory;
 import eu.europeana.metis.mediaprocessing.RdfConverterFactory;
 import eu.europeana.metis.mediaprocessing.RdfDeserializer;
 import eu.europeana.metis.mediaprocessing.model.RdfResourceEntry;
+import eu.europeana.metis.mediaprocessing.model.RdfResourceKind;
 import eu.europeana.metis.mediaprocessing.model.ResourceExtractionResult;
 import java.io.InputStream;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -25,40 +28,50 @@ class MediaExtractorImplITTest {
   private RdfDeserializer rdfDeserializer;
   private MediaExtractor mediaExtractor;
 
-  @Disabled("enable imagemagick first on metis-actions for the build.")
-  @Test
-  void testIIFExtraction() throws Exception {
-
-    rdfDeserializer = new RdfConverterFactory().createRdfDeserializer();
-    mediaExtractor = new MediaProcessorFactory().createMediaExtractor();
-    InputStream inputRdf = getClass().getClassLoader().getResourceAsStream("__files/rdf_with_iiif_sample.xml");
-    List<RdfResourceEntry> resourceEntryList = rdfDeserializer.getRemainingResourcesForMediaExtraction(inputRdf);
-    for (RdfResourceEntry resourceEntry : resourceEntryList) {
-
-      ResourceExtractionResult extractionResult = mediaExtractor.performMediaExtraction(resourceEntry, true);
-      assertNotNull(extractionResult.getMetadata());
-    }
+  private static Stream<Arguments> testMediaExtraction_ResourceAndMimeType() {
+    return Stream.of(
+        Arguments.of("__files/rdf_with_iiif_sample.xml",
+            Set.of(Pair.of("https://stacks.stanford.edu/image/iiif/zw031pj2507/zw031pj2507_0001/full/full/0/default.jpg", IIIF),
+                Pair.of("https://stacks.stanford.edu/file/zw031pj2507/zw031pj2507_0002.xml", STANDARD),
+                Pair.of("https://stacks.stanford.edu/image/iiif/zw031pj2507/zw031pj2507_0002/full/full/0/default.jpg", IIIF),
+                Pair.of("https://purl.stanford.edu/zw031pj2507", STANDARD)),
+            Set.of("application/xml", "text/html", "image/jpeg")),
+        Arguments.of("__files/rdf_with_3d_gltf.xml",
+            Set.of(Pair.of("http://cmcassociates.co.uk/Skara_Brae/landing/sb_h10_lidar.html", STANDARD),
+                Pair.of("https://raw.githubusercontent.com/europeana/metis-framework/refs/heads/develop/metis-media-service/src/test/resources/__files/3d/cube.gltf", STANDARD)),
+            Set.of("application/xhtml+xml", "model/gltf+json")),
+        Arguments.of("__files/rdf_with_3d_ply.xml",
+            Set.of(Pair.of("http://cmcassociates.co.uk/Skara_Brae/landing/sb_h10_lidar.html", STANDARD),
+                Pair.of("https://raw.githubusercontent.com/europeana/metis-framework/refs/heads/develop/metis-media-service/src/test/resources/__files/3d/cube-binary.ply", STANDARD)),
+            Set.of("application/xhtml+xml", "model/ply"))
+    );
   }
 
-  @Disabled("enable imagemagick first on metis-actions for the build.")
-  @ParameterizedTest(name = "{index} => record={0}, expected={1}")
+  @Disabled("Enable media-processing tools (image-magick, ghostscript, etc) first on metis-actions for the build.")
+  @ParameterizedTest(name = "{index} => record={0}, expected-resource{1}, expected-mimetype={2}")
   @MethodSource
-  void testCube3dExtraction_MimeType(String resourcePath, Set<String> expectedMimeTypes) throws Exception {
+  void testMediaExtraction_ResourceAndMimeType(String resourcePath,
+      Set<Pair<String, RdfResourceKind>> expectedResource,
+      Set<String> expectedMimeTypes) throws Exception {
+    // Given
     rdfDeserializer = new RdfConverterFactory().createRdfDeserializer();
     mediaExtractor = new MediaProcessorFactory().createMediaExtractor();
     InputStream inputRdf = getClass().getClassLoader().getResourceAsStream(resourcePath);
     List<RdfResourceEntry> resourceEntryList = rdfDeserializer.getRemainingResourcesForMediaExtraction(inputRdf);
-    Set<String> actualMimeTypes = HashSet.newHashSet(2);
+    Set<String> actualMimeTypes = new HashSet<>();
+    Set<Pair<String, RdfResourceKind>> actualResource = new HashSet<>();
+
     for (RdfResourceEntry resourceEntry : resourceEntryList) {
+      // When
       ResourceExtractionResult extractionResult = mediaExtractor.performMediaExtraction(resourceEntry, true);
+
+      // Then
       assertNotNull(extractionResult.getMetadata());
+      assertEquals(resourceEntry.getResourceUrl(), extractionResult.getMetadata().getResourceUrl());
       actualMimeTypes.add(extractionResult.getMetadata().getMimeType());
+      actualResource.add(Pair.of(extractionResult.getMetadata().getResourceUrl(), resourceEntry.getResourceKind()));
     }
     assertEquals(expectedMimeTypes, actualMimeTypes);
-  }
-
-  private static Stream<Arguments> testCube3dExtraction_MimeType() {
-    return Stream.of(Arguments.of("__files/record-MET-6921.xml", Set.of("application/xhtml+xml", "model/gltf+json")),
-        Arguments.of("__files/record-MET-6991.xml", Set.of("application/xhtml+xml", "model/ply")));
+    assertEquals(expectedResource, actualResource);
   }
 }
