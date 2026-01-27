500b1c52facb8d0dcfb57525fedb375a11d721b2
Jorge Ortiz <jorge.ortiz@europeana.eu>
2026-01-27 11:27:59 +0100

    MET-6921 add unit integration tests

--- metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/extraction/MediaExtractorImplITTest.java
+++ metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/extraction/MediaExtractorImplITTest.java
@@ -1,5 +1,6 @@
 package eu.europeana.metis.mediaprocessing.extraction;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import eu.europeana.metis.mediaprocessing.MediaExtractor;
@@ -9,9 +10,15 @@ import eu.europeana.metis.mediaprocessing.RdfDeserializer;
 import eu.europeana.metis.mediaprocessing.model.RdfResourceEntry;
 import eu.europeana.metis.mediaprocessing.model.ResourceExtractionResult;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class MediaExtractorImplITTest {
 
@@ -33,4 +40,25 @@ class MediaExtractorImplITTest {
     }
   }
 
+  @Disabled("enable imagemagick first on metis-actions for the build.")
+  @ParameterizedTest(name = "{index} => record={0}, expected={1}")
+  @MethodSource
+  void testCube3dExtraction_MimeType(String resourcePath, Set<String> expectedMimeTypes) throws Exception {
+    rdfDeserializer = new RdfConverterFactory().createRdfDeserializer();
+    mediaExtractor = new MediaProcessorFactory().createMediaExtractor();
+    InputStream inputRdf = getClass().getClassLoader().getResourceAsStream(resourcePath);
+    List<RdfResourceEntry> resourceEntryList = rdfDeserializer.getRemainingResourcesForMediaExtraction(inputRdf);
+    Set<String> actualMimeTypes = HashSet.newHashSet(2);
+    for (RdfResourceEntry resourceEntry : resourceEntryList) {
+      ResourceExtractionResult extractionResult = mediaExtractor.performMediaExtraction(resourceEntry, true);
+      assertNotNull(extractionResult.getMetadata());
+      actualMimeTypes.add(extractionResult.getMetadata().getMimeType());
+    }
+    assertEquals(expectedMimeTypes, actualMimeTypes);
+  }
+
+  private static Stream<Arguments> testCube3dExtraction_MimeType() {
+    return Stream.of(Arguments.of("__files/record-MET-6921.xml", Set.of("application/xhtml+xml", "model/gltf+json")),
+        Arguments.of("__files/record-MET-6991.xml", Set.of("application/xhtml+xml", "model/ply")));
+  }
 }
