8df37ce9d56d4a9d5182db971d88464afca16f30
Jorge Ortiz <jorge.ortiz@europeana.eu>
2026-01-27 15:53:01 +0100

    MET-6921 refine test including specific mimetype and urltype check

--- metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/extraction/MediaExtractorImplITTest.java
+++ metis-media-service/src/test/java/eu/europeana/metis/mediaprocessing/extraction/MediaExtractorImplITTest.java
@@ -2,6 +2,9 @@ package eu.europeana.metis.mediaprocessing.extraction;
 
 import static eu.europeana.metis.mediaprocessing.model.RdfResourceKind.IIIF;
 import static eu.europeana.metis.mediaprocessing.model.RdfResourceKind.STANDARD;
+import static eu.europeana.metis.mediaprocessing.model.UrlType.HAS_VIEW;
+import static eu.europeana.metis.mediaprocessing.model.UrlType.IS_SHOWN_AT;
+import static eu.europeana.metis.mediaprocessing.model.UrlType.IS_SHOWN_BY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -9,15 +12,16 @@ import eu.europeana.metis.mediaprocessing.MediaExtractor;
 import eu.europeana.metis.mediaprocessing.MediaProcessorFactory;
 import eu.europeana.metis.mediaprocessing.RdfConverterFactory;
 import eu.europeana.metis.mediaprocessing.RdfDeserializer;
+import eu.europeana.metis.mediaprocessing.exception.MediaProcessorException;
 import eu.europeana.metis.mediaprocessing.model.RdfResourceEntry;
 import eu.europeana.metis.mediaprocessing.model.RdfResourceKind;
 import eu.europeana.metis.mediaprocessing.model.ResourceExtractionResult;
+import eu.europeana.metis.mediaprocessing.model.UrlType;
 import java.io.InputStream;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
-import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -25,42 +29,66 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class MediaExtractorImplITTest {
 
-  private RdfDeserializer rdfDeserializer;
-  private MediaExtractor mediaExtractor;
+  private final RdfDeserializer rdfDeserializer;
+  private final MediaExtractor mediaExtractor;
+
+  private MediaExtractorImplITTest() throws MediaProcessorException {
+    rdfDeserializer = new RdfConverterFactory().createRdfDeserializer();
+    mediaExtractor = new MediaProcessorFactory().createMediaExtractor();
+  }
 
   private static Stream<Arguments> testMediaExtraction_ResourceAndMimeType() {
     return Stream.of(
         Arguments.of("__files/rdf_with_iiif_sample.xml",
-            Set.of(Pair.of("https://stacks.stanford.edu/image/iiif/zw031pj2507/zw031pj2507_0001/full/full/0/default.jpg", IIIF),
-                Pair.of("https://stacks.stanford.edu/file/zw031pj2507/zw031pj2507_0002.xml", STANDARD),
-                Pair.of("https://stacks.stanford.edu/image/iiif/zw031pj2507/zw031pj2507_0002/full/full/0/default.jpg", IIIF),
-                Pair.of("https://purl.stanford.edu/zw031pj2507", STANDARD)),
-            Set.of("application/xml", "text/html", "image/jpeg")),
+            Set.of(
+                new ExtractionParameters(
+                    "https://stacks.stanford.edu/image/iiif/zw031pj2507/zw031pj2507_0001/full/full/0/default.jpg",
+                    "image/jpeg", Set.of(IS_SHOWN_BY), IIIF),
+                new ExtractionParameters("https://stacks.stanford.edu/file/zw031pj2507/zw031pj2507_0002.xml",
+                    "application/xml", Set.of(HAS_VIEW), STANDARD),
+                new ExtractionParameters(
+                    "https://stacks.stanford.edu/image/iiif/zw031pj2507/zw031pj2507_0002/full/full/0/default.jpg",
+                    "image/jpeg", Set.of(HAS_VIEW), IIIF),
+                new ExtractionParameters("https://purl.stanford.edu/zw031pj2507", "text/html", Set.of(IS_SHOWN_AT), STANDARD)
+            )
+        ),
         Arguments.of("__files/rdf_with_3d_gltf.xml",
-            Set.of(Pair.of("http://cmcassociates.co.uk/Skara_Brae/landing/sb_h10_lidar.html", STANDARD),
-                Pair.of("https://raw.githubusercontent.com/europeana/metis-framework/refs/heads/develop/metis-media-service/src/test/resources/__files/3d/cube.gltf", STANDARD)),
-            Set.of("application/xhtml+xml", "model/gltf+json")),
+            Set.of(
+                new ExtractionParameters("http://cmcassociates.co.uk/Skara_Brae/landing/sb_h10_lidar.html",
+                    "application/xhtml+xml", Set.of(IS_SHOWN_BY), STANDARD),
+                new ExtractionParameters(
+                    "https://raw.githubusercontent.com/europeana/metis-framework/refs/heads/develop/metis-media-service/src/test/resources/__files/3d/cube.gltf",
+                    "model/gltf+json", Set.of(IS_SHOWN_AT), STANDARD)
+            )
+        ),
         Arguments.of("__files/rdf_with_3d_ply.xml",
-            Set.of(Pair.of("http://cmcassociates.co.uk/Skara_Brae/landing/sb_h10_lidar.html", STANDARD),
-                Pair.of("https://raw.githubusercontent.com/europeana/metis-framework/refs/heads/develop/metis-media-service/src/test/resources/__files/3d/cube-binary.ply", STANDARD)),
-            Set.of("application/xhtml+xml", "model/ply"))
+            Set.of(
+                new ExtractionParameters("http://cmcassociates.co.uk/Skara_Brae/landing/sb_h10_lidar.html",
+                    "application/xhtml+xml", Set.of(IS_SHOWN_BY), STANDARD),
+                new ExtractionParameters(
+                    "https://raw.githubusercontent.com/europeana/metis-framework/refs/heads/develop/metis-media-service/src/test/resources/__files/3d/cube-binary.ply",
+                    "model/ply", Set.of(IS_SHOWN_AT), STANDARD)
+            )
+        )
     );
   }
 
+  /**
+   * The type Extraction parameters.
+   */
+  record ExtractionParameters(String resourceURL, String mimeType, Set<UrlType> urlType, RdfResourceKind resourceKind) {
+    //used internally for testing purposes
+  }
+
   @Disabled("Enable media-processing tools (image-magick, ghostscript, etc) first on metis-actions for the build.")
-  @ParameterizedTest(name = "{index} => record={0}, expected-resource{1}, expected-mimetype={2}")
+  @ParameterizedTest(name = "{index} => record={0}, expected-resource{1}")
   @MethodSource
   void testMediaExtraction_ResourceAndMimeType(String resourcePath,
-      Set<Pair<String, RdfResourceKind>> expectedResource,
-      Set<String> expectedMimeTypes) throws Exception {
+      Set<ExtractionParameters> expectedExtractionParameters) throws Exception {
     // Given
-    rdfDeserializer = new RdfConverterFactory().createRdfDeserializer();
-    mediaExtractor = new MediaProcessorFactory().createMediaExtractor();
     InputStream inputRdf = getClass().getClassLoader().getResourceAsStream(resourcePath);
     List<RdfResourceEntry> resourceEntryList = rdfDeserializer.getRemainingResourcesForMediaExtraction(inputRdf);
-    Set<String> actualMimeTypes = new HashSet<>();
-    Set<Pair<String, RdfResourceKind>> actualResource = new HashSet<>();
-
+    Set<ExtractionParameters> actualExtractionParameters = new HashSet<>();
     for (RdfResourceEntry resourceEntry : resourceEntryList) {
       // When
       ResourceExtractionResult extractionResult = mediaExtractor.performMediaExtraction(resourceEntry, true);
@@ -68,10 +96,9 @@ class MediaExtractorImplITTest {
       // Then
       assertNotNull(extractionResult.getMetadata());
       assertEquals(resourceEntry.getResourceUrl(), extractionResult.getMetadata().getResourceUrl());
-      actualMimeTypes.add(extractionResult.getMetadata().getMimeType());
-      actualResource.add(Pair.of(extractionResult.getMetadata().getResourceUrl(), resourceEntry.getResourceKind()));
+      actualExtractionParameters.add(new ExtractionParameters(extractionResult.getMetadata().getResourceUrl(),
+          extractionResult.getMetadata().getMimeType(), resourceEntry.getUrlTypes(), resourceEntry.getResourceKind()));
     }
-    assertEquals(expectedMimeTypes, actualMimeTypes);
-    assertEquals(expectedResource, actualResource);
+    assertEquals(expectedExtractionParameters, actualExtractionParameters);
   }
 }
