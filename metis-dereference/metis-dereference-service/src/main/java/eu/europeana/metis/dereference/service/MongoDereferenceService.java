61d9890913a77f157e0fdb83e8ac8c12ad198a52
Simon Tzanakis <simon.tzanakis@europeana.eu>
2026-04-09 09:56:51 +0200

    MET-6398 Use escapeJava in logs (#779)
    * MET-6398 Use escapeJava in logs
    
    * MET-6398 Process review
    
    * MET-6398 Process review

--- metis-dereference/metis-dereference-service/src/main/java/eu/europeana/metis/dereference/service/MongoDereferenceService.java
+++ metis-dereference/metis-dereference-service/src/main/java/eu/europeana/metis/dereference/service/MongoDereferenceService.java
@@ -1,6 +1,6 @@
 package eu.europeana.metis.dereference.service;
 
-import static eu.europeana.metis.utils.CommonStringValues.sanitizeCRLF;
+import static org.apache.commons.text.StringEscapeUtils.escapeJava;
 
 import eu.europeana.enrichment.api.external.DereferenceResultStatus;
 import eu.europeana.enrichment.api.external.model.Concept;
@@ -292,7 +292,7 @@ public class MongoDereferenceService implements DereferenceService {
 
         // Evaluate and return the result.
         if (originalEntity == null && LOGGER.isInfoEnabled()) {
-            LOGGER.info("No entity XML for uri {}", sanitizeCRLF(resourceId));
+            LOGGER.info("No entity XML for uri {}", escapeJava(resourceId));
         }
         final DereferenceResultStatus dereferenceResultStatus = originalEntity == null ?
             DereferenceResultStatus.NO_ENTITY_FOR_VOCABULARY : DereferenceResultStatus.SUCCESS;
