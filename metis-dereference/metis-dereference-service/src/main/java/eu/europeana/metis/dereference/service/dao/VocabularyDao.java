5438cd17fa1b26c6e5f636e09b5782200539433c
Simon Tzanakis <simon.tzanakis@europeana.eu>
2026-04-30 19:07:57 +0200

    Try regex for vocabulary URI search

--- metis-dereference/metis-dereference-service/src/main/java/eu/europeana/metis/dereference/service/dao/VocabularyDao.java
+++ metis-dereference/metis-dereference-service/src/main/java/eu/europeana/metis/dereference/service/dao/VocabularyDao.java
@@ -48,7 +48,7 @@ public class VocabularyDao {
   public List<Vocabulary> getByUriSearch(String searchString) {
     final Pattern pattern = Pattern.compile(Pattern.quote(searchString));
     final Query<Vocabulary> query = datastore.find(Vocabulary.class);
-    query.filter(Filters.eq("uris", pattern));
+    query.filter(Filters.regex("uris", pattern));
     return getListOfQueryRetryable(query);
   }
 
