df61f7a6e59cea59233b0e2a0647fb17b466772b
Jorge Ortiz <jorge.ortiz@europeana.eu>
2026-06-22 15:25:42 +0200

    MET-7115 fix package namespace

--- metis-indexing/src/test/java/eu/europeana/indexing/base/IndexingTestUtils.java
+++ metis-indexing/src/test/java/eu/europeana/indexing/base/IndexingTestUtils.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import org.apache.solr.client.solrj.SolrClient;
 
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.request.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
 
