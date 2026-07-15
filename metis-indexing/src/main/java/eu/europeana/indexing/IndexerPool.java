9e0f8a4a901d730920322ef187225ca3063f7b62
Simon Tzanakis <simon.tzanakis@europeana.eu>
2026-07-15 14:42:24 +0200

    MET-7204 Update IndexerPool to include new methods required for metis-sandbox

--- metis-indexing/src/main/java/eu/europeana/indexing/IndexerPool.java
+++ metis-indexing/src/main/java/eu/europeana/indexing/IndexerPool.java
@@ -4,10 +4,14 @@ import eu.europeana.indexing.exception.IndexerRelatedIndexingException;
 import eu.europeana.indexing.exception.IndexingException;
 import eu.europeana.indexing.exception.RecordRelatedIndexingException;
 import eu.europeana.indexing.exception.SetupRelatedIndexingException;
+import eu.europeana.indexing.tiers.model.TierResults;
 import eu.europeana.metis.schema.jibx.RDF;
 import eu.europeana.metis.utils.DepublicationReason;
 import java.io.Closeable;
+import java.io.InputStream;
 import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
 import org.apache.commons.pool2.BasePooledObjectFactory;
 import org.apache.commons.pool2.PooledObject;
 import org.apache.commons.pool2.impl.DefaultPooledObject;
@@ -17,14 +21,13 @@ import org.slf4j.LoggerFactory;
 
 /**
  * <p>
- * This class is a utilities object providing the functionality of a pool of indexer objects, all
- * created from the same factory/settings. This class is designed to save the time otherwise spent
- * on repeatedly creating and destroying indexers.
+ * This class is a utilities object providing the functionality of a pool of indexer objects, all created from the same
+ * factory/settings. This class is designed to save the time otherwise spent on repeatedly creating and destroying indexers.
  * </p>
  * <p>
- * This pool can have an unlimited number of indexers, that will be created automatically whenever
- * they are needed (i.e. when all other indexers are in use already). This thread pool implements
- * the automatic destruction of idle indexers, given a certain permissible idle time.
+ * This pool can have an unlimited number of indexers, that will be created automatically whenever they are needed (i.e. when all
+ * other indexers are in use already). This thread pool implements the automatic destruction of idle indexers, given a certain
+ * permissible idle time.
  * </p>
  * <p>
  * This class is thread-safe.
@@ -40,10 +43,9 @@ public class IndexerPool implements Closeable {
    * Constructor.
    *
    * @param indexingSettings The  settings with which to create the indexer instances in the pool.
-   * @param maxIdleTimeForIndexerInSecs The idle time after which an indexer is eligible for
-   * destruction, in seconds.
-   * @param idleTimeCheckIntervalInSecs The interval with which we check the idle time of indexers
-   * to decide whether to destroy them, in seconds.
+   * @param maxIdleTimeForIndexerInSecs The idle time after which an indexer is eligible for destruction, in seconds.
+   * @param idleTimeCheckIntervalInSecs The interval with which we check the idle time of indexers to decide whether to destroy
+   * them, in seconds.
    */
   public IndexerPool(IndexingSettings indexingSettings, long maxIdleTimeForIndexerInSecs,
       long idleTimeCheckIntervalInSecs) {
@@ -55,10 +57,9 @@ public class IndexerPool implements Closeable {
    * Constructor.
    *
    * @param indexerFactory The factory from which to create the indexer instances in the pool.
-   * @param maxIdleTimeForIndexerInSecs The idle time after which an indexer is eligible for
-   * destruction, in seconds.
-   * @param idleTimeCheckIntervalInSecs The interval with which we check the idle time of indexers
-   * to decide whether to destroy them, in seconds.
+   * @param maxIdleTimeForIndexerInSecs The idle time after which an indexer is eligible for destruction, in seconds.
+   * @param idleTimeCheckIntervalInSecs The interval with which we check the idle time of indexers to decide whether to destroy
+   * them, in seconds.
    */
   public IndexerPool(IndexerFactory<?> indexerFactory, long maxIdleTimeForIndexerInSecs,
       long idleTimeCheckIntervalInSecs) {
@@ -91,7 +92,10 @@ public class IndexerPool implements Closeable {
    * @throws IndexingException In case a problem occurred during indexing. indexer.
    */
   public void index(String stringRdfRecord, IndexingProperties indexingProperties) throws IndexingException {
-    indexRecord(indexer ->{ indexer.index(stringRdfRecord, indexingProperties); return true;});
+    executeIndexCommand(indexer -> {
+      indexer.index(stringRdfRecord, indexingProperties);
+      return true;
+    });
   }
 
   /**
@@ -103,7 +107,7 @@ public class IndexerPool implements Closeable {
    * @throws IndexingException the indexing exception.
    */
   public boolean indexTombstone(String rdfAbout, DepublicationReason depublicationReason) throws IndexingException {
-    return indexRecord(indexer -> indexer.indexTombstone(rdfAbout, depublicationReason));
+    return executeIndexCommand(indexer -> indexer.indexTombstone(rdfAbout, depublicationReason));
   }
 
   /**
@@ -114,7 +118,7 @@ public class IndexerPool implements Closeable {
    * @throws IndexingException the indexing exception
    */
   public boolean removeTombstone(String rdfAbout) throws IndexingException {
-    return indexRecord(indexer -> indexer.removeTombstone(rdfAbout));
+    return executeIndexCommand(indexer -> indexer.removeTombstone(rdfAbout));
   }
 
   /**
@@ -127,7 +131,10 @@ public class IndexerPool implements Closeable {
    * @throws IndexingException In case a problem occurred during indexing. indexer.
    */
   public void indexRdf(RDF stringRdfRecord, IndexingProperties indexingProperties) throws IndexingException {
-    indexRecord(indexer -> {indexer.indexRdf(stringRdfRecord, indexingProperties); return true;});
+    executeIndexCommand(indexer -> {
+      indexer.indexRdf(stringRdfRecord, indexingProperties);
+      return true;
+    });
   }
 
   /**
@@ -138,10 +145,51 @@ public class IndexerPool implements Closeable {
    * @throws IndexingException In case something went wrong.
    */
   public boolean removeRecord(String stringRdfRecord) throws IndexingException {
-    return indexRecord(indexer -> indexer.remove(stringRdfRecord));
+    return executeIndexCommand(indexer -> indexer.remove(stringRdfRecord));
   }
 
-  private boolean indexRecord(IndexTask indexTask) throws IndexingException {
+  /**
+   * This method indexes a record provided in the form of an InputStream and calculates its tier classifications, such as media
+   * tier and metadata tier values. It uses an indexer from the pool to perform the operation and returns the tier result values.
+   *
+   * @param recordContent The InputStream containing the record content to be indexed.
+   * @param indexingProperties The properties and parameters applied during the indexing operation.
+   * @return A {@code TierResults} object that encapsulates the calculated media and metadata tier classifications.
+   * @throws IndexingException If any issue occurs while performing the indexing operation.
+   */
+  public TierResults indexAndGetTierCalculations(InputStream recordContent, IndexingProperties indexingProperties)
+      throws IndexingException {
+    return executeIndexCommand(indexer -> indexer.indexAndGetTierCalculations(recordContent, indexingProperties));
+  }
+
+  /**
+   * Removes all records from the specified dataset that were created/updated less than the maximum record date.
+   *
+   * @param datasetId The identifier of the dataset from which records will be removed.
+   * @param maxRecordDate The maximum date of records to be removed; any record with a creation date on or before this date will
+   * be removed.
+   * @return The number of records that were successfully removed.
+   * @throws IndexingException If an error occurs during the removal operation.
+   */
+  public int removeAll(String datasetId, Instant maxRecordDate) throws IndexingException {
+    return executeIndexCommand(indexer -> indexer.removeAll(datasetId, Date.from(maxRecordDate)));
+  }
+
+  /**
+   * Counts the number of records in the specified dataset that have a timestamp updated less than the given maximum record date.
+   * If the maximum record date is null, all records in the dataset will be counted.
+   *
+   * @param datasetId The identifier of the dataset from which records will be counted. Must not be null.
+   * @param maxRecordDate The maximum date used as a cutoff for filtering records. If null, all records in the dataset will be
+   * included in the count.
+   * @return The total number of records that satisfy the criteria.
+   * @throws IndexingException If an error occurs while performing the counting operation.
+   */
+  public long countRecords(String datasetId, Instant maxRecordDate) throws IndexingException {
+    return executeIndexCommand(indexer -> indexer.countRecords(datasetId, Date.from(maxRecordDate)));
+  }
+
+  private <R> R executeIndexCommand(IndexTask<R> indexTask) throws IndexingException {
 
     // Obtain indexer from the pool.
     final Indexer<?> indexer;
@@ -153,7 +201,7 @@ public class IndexerPool implements Closeable {
       throw new IndexerRelatedIndexingException("Error while obtaining indexer from the pool.", e);
     }
 
-    boolean taskResult = false;
+    R taskResult = null;
     // Perform indexing and release indexer.
     try {
       taskResult = indexTask.performTask(indexer);
@@ -189,8 +237,9 @@ public class IndexerPool implements Closeable {
   }
 
   @FunctionalInterface
-  private interface IndexTask {
-    boolean performTask(Indexer<?> indexer) throws IndexingException;
+  private interface IndexTask<R> {
+
+    R performTask(Indexer<?> indexer) throws IndexingException;
   }
 
   private static class PooledIndexerFactory extends BasePooledObjectFactory<Indexer<?>> {
